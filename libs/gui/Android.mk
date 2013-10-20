e49bf74584587d6b44b3a6e432295e5d3c601c23
OliverG96 <oliverarafo@gmail.com>
2013-10-20 12:01:57 +0100

    native: Add support for STE multimedia - Part 2
    Enourmous thanks to tecul for doing the perfect port and making everything clean and bugless.
    
    Change-Id: Id14f2289fb0dc28e370d56530e5ae7bc86923a25

--- libs/gui/Android.mk
+++ libs/gui/Android.mk
@@ -36,6 +36,7 @@ LOCAL_SHARED_LIBRARIES := \
 	libsync \
 	libui \
 	libutils \
+	libhardware \
 	liblog
 
 # Executed only on QCOM BSPs
@@ -67,7 +68,7 @@ ifeq ($(TARGET_QCOM_DISPLAY_VARIANT), legacy)
 	LOCAL_CFLAGS += -DDONT_USE_FENCE_SYNC
 endif
 ifeq ($(TARGET_TOROPLUS_RADIO), true)
-	LOCAL_CFLAGS += -DTOROPLUS_RADIO
+        LOCAL_CFLAGS += -DTOROPLUS_RADIO
 endif
 
 include $(BUILD_SHARED_LIBRARY)
--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2760,6 +2760,7 @@ class GraphicProducerWrapper : public BBinder, public MessageHandler {
     uint32_t code;
     Parcel const* data;
     Parcel* reply;
+    Mutex mLock;
 
     enum {
         MSG_API_CALL,
@@ -2772,6 +2773,7 @@ class GraphicProducerWrapper : public BBinder, public MessageHandler {
      */
     virtual status_t transact(uint32_t code,
             const Parcel& data, Parcel* reply, uint32_t flags) {
+        mLock.lock();
         this->code = code;
         this->data = &data;
         this->reply = reply;
@@ -2784,6 +2786,7 @@ class GraphicProducerWrapper : public BBinder, public MessageHandler {
             looper->sendMessage(this, Message(MSG_API_CALL));
             barrier.wait();
         }
+        mLock.unlock();
         return NO_ERROR;
     }
 
