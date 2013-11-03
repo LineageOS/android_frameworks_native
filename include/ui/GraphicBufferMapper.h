b107d08a439ee09691fe176aab059ec31a8401f5
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-03 03:07:35 +0000

    ui: exynos4: getphys is common
    Conflicts:
    
    	include/ui/GraphicBufferMapper.h
    	libs/ui/GraphicBufferMapper.cpp
    
    Change-Id: I8b5e1b0b22cdcd2ede0fe14f537b1bd631b98504

--- include/ui/GraphicBufferMapper.h
+++ include/ui/GraphicBufferMapper.h
@@ -58,7 +58,7 @@ public:
 
     status_t unlockAsync(buffer_handle_t handle, int *fenceFd);
     
-#ifdef EXYNOS4210_ENHANCEMENTS
+#ifdef EXYNOS4_ENHANCEMENTS
     status_t getphys(buffer_handle_t handle, void** paddr);
 #endif
 
--- libs/ui/GraphicBufferMapper.cpp
+++ libs/ui/GraphicBufferMapper.cpp
@@ -171,7 +171,7 @@ status_t GraphicBufferMapper::unlockAsync(buffer_handle_t handle, int *fenceFd)
     return err;
 }
 
-#ifdef EXYNOS4210_ENHANCEMENTS
+#ifdef EXYNOS4_ENHANCEMENTS
 status_t GraphicBufferMapper::getphys(buffer_handle_t handle, void** paddr)
 {
     status_t err;
