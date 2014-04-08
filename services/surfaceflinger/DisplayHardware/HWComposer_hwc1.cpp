7fcabfc419b0a543d7848a506edcaa2500f8b134
Mykola Ostrovskyy <mykola@ti.com>
2014-04-08 14:36:57 -0500

    HWComposer: Use native FB format for HWC 1.1
    Until HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED is supported by
    EGL, we have to hard-code FB pixel format.
    
    Change-Id: I0f654c894bbfa82851bb03b85ad59e7c7305f309
    Signed-off-by: Mykola Ostrovskyy <mykola@ti.com>

--- services/surfaceflinger/DisplayHardware/HWComposer_hwc1.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer_hwc1.cpp
@@ -433,7 +433,12 @@ status_t HWComposer::queryDisplayProperties(int disp) {
     }
 
     // FIXME: what should we set the format to?
+#ifdef OMAP_ENHANCEMENT
+    // Use pixel format native to DSS HW
+    mDisplayData[disp].format = HAL_PIXEL_FORMAT_BGRA_8888;
+#else
     mDisplayData[disp].format = HAL_PIXEL_FORMAT_RGBA_8888;
+#endif
     mDisplayData[disp].connected = true;
     return NO_ERROR;
 }
@@ -862,7 +867,12 @@ int HWComposer::getVisualID() const {
         // FIXME: temporary hack until HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED
         // is supported by the implementation. we can only be in this case
         // if we have HWC 1.1
+#ifdef OMAP_ENHANCEMENT
+        // Use pixel format native to DSS HW
+        return HAL_PIXEL_FORMAT_BGRA_8888;
+#else
         return HAL_PIXEL_FORMAT_RGBA_8888;
+#endif
         //return HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED;
     } else {
         return mFbDev->format;
