262978bf53f21077204d30569b96c801532e25fe
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-11-18 08:38:33 +0700

    Bring back support for glReadPixels screenshot path
    Squashed commit of the following:
    
    commit 012d3fe41d1d6cd38a0858b59145e9a4447641fa
    Author: Hashcode <hashcode0f@gmail.com>
    Date:   Sun Dec 8 19:36:50 2013 +0000
    
        sf: Always use opengles for screen capture
    
        Go back to the usage of GRALLOC_USAGE_HW_TEXTURE and GRALLOC_USAGE_HW_RENDERER
        in captureScreenImplLocked regardless of useReadPixels value
    
        This fixes the EGL_NO_IMAGE_KHR error returned from
        eglCreateImageKHR (blank images returned from screenshot path)
    
        Change-Id: I62fe90a081607b9e89c67f3dcfd34c84efc89d35
    
    commit 4866ddf98ac98d8e22a1cd6a21894bb17f274588
    Author: Ricardo Cerqueira <cyanogenmod@cerqueira.org>
    Date:   Thu Oct 31 03:53:39 2013 +0000
    
        Revert "remove support for glReadPixels screenshot path"
    
        This reverts commit 3ca76f416bc8665a97636ca8a2d0128b9da9d92c.
    
        Conflicts:
        	include/gui/ISurfaceComposer.h
        	libs/gui/ISurfaceComposer.cpp
        	libs/gui/SurfaceComposerClient.cpp
        	services/surfaceflinger/SurfaceFlinger.cpp
        	services/surfaceflinger/SurfaceFlinger.h
    
        Change-Id: I8c239e533757af770e418dbb198f5a86c736961f
    
    Change-Id: I8c239e533757af770e418dbb198f5a86c736961f

--- libs/gui/SurfaceComposerClient.cpp
+++ libs/gui/SurfaceComposerClient.cpp
@@ -860,7 +860,8 @@ status_t ScreenshotClient::capture(
     sp<ISurfaceComposer> s(ComposerService::getComposerService());
     if (s == NULL) return NO_INIT;
     return s->captureScreen(display, producer, sourceCrop,
-            reqWidth, reqHeight, minLayerZ, maxLayerZ, useIdentityTransform);
+            reqWidth, reqHeight, minLayerZ, maxLayerZ, useIdentityTransform,
+            ISurfaceComposer::eRotateNone, false);
 }
 
 ScreenshotClient::ScreenshotClient()
@@ -898,7 +899,7 @@ status_t ScreenshotClient::update(const sp<IBinder>& display,
 
     status_t err = s->captureScreen(display, mProducer, sourceCrop,
             reqWidth, reqHeight, minLayerZ, maxLayerZ, useIdentityTransform,
-            static_cast<ISurfaceComposer::Rotation>(rotation));
+            static_cast<ISurfaceComposer::Rotation>(rotation), true);
 
     if (err == NO_ERROR) {
         err = mCpuConsumer->lockNextBuffer(&mBuffer);
