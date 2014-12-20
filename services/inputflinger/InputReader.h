42e33a02d5a2d41f96d8abf7d7b3e8f70512edc4
nadlabak <pavel@doshaska.net>
2014-12-21 00:10:54 +0200

    Forward port 'Swap volume buttons' (2/3)
    [mikeioannina]: Adjust for 5.0 changes
    [LuK1337]: Adjust for 8.1 changes
    
    Change-Id: I5ed4ae2b7e69e2ada067ed1d3524b3d3fad30e2e

--- services/inputflinger/InputReader.h
+++ services/inputflinger/InputReader.h
@@ -90,6 +90,9 @@ struct InputReaderConfiguration {
         // The set of disabled input devices (disabledDevices) has changed.
         CHANGE_ENABLED_STATE = 1 << 9,
 
+        // Volume keys rotation option changed.
+        CHANGE_VOLUME_KEYS_ROTATION = 1 << 10,
+
         // All devices must be reopened.
         CHANGE_MUST_REOPEN = 1 << 31,
     };
@@ -183,6 +186,10 @@ struct InputReaderConfiguration {
     // The set of currently disabled input devices.
     SortedVector<int32_t> disabledDevices;
 
+    // Remap volume keys according to display rotation
+    // 0 - disabled, 1 - phone or hybrid rotation mode, 2 - tablet rotation mode
+    int volumeKeysRotationMode;
+
     InputReaderConfiguration() :
             virtualKeyQuietTime(0),
             pointerVelocityControlParameters(1.0f, 500.0f, 3000.0f, 3.0f),
@@ -199,7 +206,8 @@ struct InputReaderConfiguration {
             pointerGestureSwipeMaxWidthRatio(0.25f),
             pointerGestureMovementSpeedRatio(0.8f),
             pointerGestureZoomSpeedRatio(0.3f),
-            showTouches(false) { }
+            showTouches(false),
+            volumeKeysRotationMode(0) { }
 
     bool getDisplayViewport(ViewportType viewportType, const String8* displayId,
             DisplayViewport* outViewport) const;
@@ -1101,7 +1109,8 @@ private:
     uint32_t mSource;
     int32_t mKeyboardType;
 
-    int32_t mOrientation; // orientation for dpad keys
+    int32_t mRotationMapOffset; // determines if and how volume keys rotate
+    int32_t mOrientation; // orientation for dpad and volume keys
 
     Vector<KeyDown> mKeyDowns; // keys that are down
     int32_t mMetaState;
