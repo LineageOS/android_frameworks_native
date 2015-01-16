4fe10cf348227a64a86054b39088f2ca8c72a62e
ramsudharsan <ramsudharsanm@gmail.com>
2015-01-16 10:42:28 +0000

    libEGL: Google bug 10194508l[Global] && added support old EGL libs
    Most older blobs don't have the EGL_KHR_gl_colorspace extension and need this workaround. And this bug is global as of now. Also Added support old EGL libraries which needs FNW to ride up!
    
    Part of this commit is referenced from: https://gerrit.slimroms.net/#/c/23348/
    
    Change-Id: I6419c72e01aecef37244e2553bd842f970b2cb3f

--- services/surfaceflinger/DisplayDevice.cpp
+++ services/surfaceflinger/DisplayDevice.cpp
@@ -27,6 +27,10 @@
 #include <ui/DisplayInfo.h>
 #include <ui/PixelFormat.h>
 
+#ifdef EGL_NEEDS_FNW
+#include <ui/FramebufferNativeWindow.h>
+#endif
+
 #include <gui/Surface.h>
 
 #include <hardware/gralloc.h>
@@ -78,7 +82,11 @@ DisplayDevice::DisplayDevice(
       mActiveConfig(0)
 {
     mNativeWindow = new Surface(producer, false);
+    #ifndef EGL_NEEDS_FNW
     ANativeWindow* const window = mNativeWindow.get();
+    #else
+    ANativeWindow* const window = new FramebufferNativeWindow();
+    #endif
 
     /*
      * Create our display's surface
@@ -450,7 +458,11 @@ void DisplayDevice::setDisplaySize(const int newWidth, const int newHeight) {
 
     mDisplaySurface->resizeBuffers(newWidth, newHeight);
 
+    #ifndef EGL_NEEDS_FNW
     ANativeWindow* const window = mNativeWindow.get();
+    #else
+    ANativeWindow* const window = new FramebufferNativeWindow();
+    #endif
     mSurface = eglCreateWindowSurface(mDisplay, mConfig, window, NULL);
     eglQuerySurface(mDisplay, mSurface, EGL_WIDTH,  &mDisplayWidth);
     eglQuerySurface(mDisplay, mSurface, EGL_HEIGHT, &mDisplayHeight);
