3dde3a50f4443700bd33f296cb55fe1e5ae81bfd
Steve Kondik <steve@cyngn.com>
2015-10-12 13:44:11 -1000

    surfaceflinger: Clean up use of QC extended API
    gui: only support tertiary display if QTI_BSP is defined
    
    Non-caf display HALs crap out with this
    
    surfaceflinger: Correct QTI_BSP ifdef
    
    * Should be #ifdef, not #if
    
    REF: CRACKLING-938
    
    sf: Fix blending mode for legacy BSP
    
     * After 0c5f3af, glitches when blending is observed on
       A family devices. Modify ifdef to not include QCOM_BSP_LEGACY.
    
    surfaceflinger: Fix the fix of the opaque check fix
    
     * Use the right define.
    
    Change-Id: I45deaffc0d5efd8ffa643120b0dea3aab461d70f
    
    Build fix after CAF merge
    
    Change-Id: I5a7cfbe74d6fecabefb0281b64daa5291d3f728d

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -38,11 +38,7 @@ LOCAL_SRC_FILES := \
     RenderEngine/GLES10RenderEngine.cpp \
     RenderEngine/GLES11RenderEngine.cpp \
     RenderEngine/GLES20RenderEngine.cpp \
-    DisplayUtils.cpp \
-    ExSurfaceFlinger/ExLayer.cpp \
-    ExSurfaceFlinger/ExSurfaceFlinger.cpp \
-    ExSurfaceFlinger/ExVirtualDisplaySurface.cpp \
-    ExSurfaceFlinger/ExHWComposer.cpp
+    DisplayUtils.cpp
 
 LOCAL_C_INCLUDES := \
 	frameworks/native/vulkan/include \
@@ -160,9 +156,14 @@ ifeq ($(TARGET_USES_QCOM_BSP), true)
   else
     LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qcom/display
   endif
-    LOCAL_SHARED_LIBRARIES += libqdutils
-    LOCAL_SHARED_LIBRARIES += libqdMetaData
-    LOCAL_CFLAGS += -DQTI_BSP
+  LOCAL_SHARED_LIBRARIES += libqdutils
+  LOCAL_SHARED_LIBRARIES += libqdMetaData
+  LOCAL_CFLAGS += -DQTI_BSP
+  LOCAL_SRC_FILES += \
+    ExSurfaceFlinger/ExLayer.cpp \
+    ExSurfaceFlinger/ExSurfaceFlinger.cpp \
+    ExSurfaceFlinger/ExVirtualDisplaySurface.cpp \
+    ExSurfaceFlinger/ExHWComposer.cpp
 endif
 
 LOCAL_MODULE := libsurfaceflinger
