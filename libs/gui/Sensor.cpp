eed05ce980d89f74475d3b8b85fae31737427844
Christopher N. Hesse <raymanfx@gmail.com>
2015-11-29 00:39:51 +0100

    DO NOT MERGE: sensor: Skip permission request
    We need to find a better way to work around this issue.
    
    Change-Id: I698f4129e71b683f6f063f00da79f32a5f521149

--- libs/gui/Sensor.cpp
+++ libs/gui/Sensor.cpp
@@ -262,6 +262,7 @@ Sensor::Sensor(struct sensor_t const* hwSensor, int halVersion)
         }
     }
 
+#ifndef NO_SENSOR_PERMISSION_CHECK
     if (mRequiredPermission.length() > 0) {
         // If the sensor is protected by a permission we need to know if it is
         // a runtime one to determine whether we can use the permission cache.
@@ -272,6 +273,7 @@ Sensor::Sensor(struct sensor_t const* hwSensor, int halVersion)
                     String16(mRequiredPermission));
         }
     }
+#endif
 }
 
 Sensor::~Sensor()
