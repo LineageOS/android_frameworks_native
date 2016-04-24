fe708d1abc36ac2bfb8ee77f9eac934d11e5e821
Christopher N. Hesse <raymanfx@gmail.com>
2016-04-24 19:30:35 +0200

    surfaceflinger: Add support for Exynos BGRA mixer
    Change-Id: Ibc9f784b35a7bd6d8cc6b13ff68b12418cce98c5

--- services/surfaceflinger/DisplayHardware/HWComposer.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer.cpp
@@ -879,7 +879,11 @@ int HWComposer::getVisualID() const {
         // FIXME: temporary hack until HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED
         // is supported by the implementation. we can only be in this case
         // if we have HWC 1.1
+#ifdef USE_BGRA_8888
+        return HAL_PIXEL_FORMAT_BGRA_8888;
+#else
         return HAL_PIXEL_FORMAT_RGBA_8888;
+#endif
         //return HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED;
     } else {
         return mFbDev->format;
