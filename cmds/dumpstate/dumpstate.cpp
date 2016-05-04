32caf1d80b689d7763a7cf853880987ccc24a479
Elliott Hughes <enh@google.com>
2016-05-04 14:03:54 -0700

    Switch to more conventional ps arguments in dumpstate.
    I'll come back and remove the "toybox" once I've switched the symlink
    from toolbox to toybox...
    
    Change-Id: I00f848ab4dade8650726b054429a2926d3d72018

--- cmds/dumpstate/dumpstate.cpp
+++ cmds/dumpstate/dumpstate.cpp
@@ -429,7 +429,8 @@ static void dumpstate() {
     dump_file("KERNEL CPUFREQ", "/sys/devices/system/cpu/cpu0/cpufreq/stats/time_in_state");
     dump_file("KERNEL SYNC", "/d/sync");
 
-    run_command("PROCESSES AND THREADS", 10, "ps", "-Z", "-t", "-p", "-P", NULL);
+    run_command("PROCESSES AND THREADS", 10, "toybox", "ps", "-A", "-T", "-Z",
+                "-O", "pri,nice,rtprio,sched,pcy", NULL);
     run_command("LIBRANK", 10, "librank", NULL);
 
     do_dmesg();
