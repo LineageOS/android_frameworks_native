4f89564970a9e8187a1eaa37000bcb50d6dfadc4
Elliott Hughes <enh@google.com>
2016-10-13 17:21:46 -0700

    Rely on the platform -std default.
    Bug: http://b/32019064
    Test: builds
    Change-Id: I03f8e660cfff247e061c838d74761a4e61ae15e8

--- services/surfaceflinger/tests/Android.mk
+++ services/surfaceflinger/tests/Android.mk
@@ -3,8 +3,6 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 
-LOCAL_CFLAGS := -std=c++14
-
 LOCAL_MODULE := SurfaceFlinger_test
 
 LOCAL_MODULE_TAGS := tests
