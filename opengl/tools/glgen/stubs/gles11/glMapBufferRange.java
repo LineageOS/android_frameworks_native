f7d15c5e36607cd75a92c258f01f7a2c76cd87b7
Romain Guy <romainguy@google.com>
2016-11-30 17:16:12 -0800

    Guarantee that glMapBufferRange/glGetBufferPointv return a ByteBuffer
    Bug: 24950082
    Test: CtsGraphicsTestCases
    Change-Id: I3774940392ba39ac92fcf680a7b6786fe61876a3

--- opengl/tools/glgen/stubs/gles11/glMapBufferRange.java
+++ opengl/tools/glgen/stubs/gles11/glMapBufferRange.java
@@ -1,5 +1,9 @@
     // C function GLvoid * glMapBufferRange ( GLenum target, GLintptr offset, GLsizeiptr length, GLbitfield access )
 
+    /**
+     * The {@link java.nio.Buffer} instance returned by this method is guaranteed
+     * to be an instance of {@link java.nio.ByteBuffer}.
+     */
     public static native java.nio.Buffer glMapBufferRange(
         int target,
         int offset,
