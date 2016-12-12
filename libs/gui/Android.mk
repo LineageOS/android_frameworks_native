53c82c269692442f1e6456127184f3afa7d1d568
Jesse Hall <jessehall@google.com>
2016-12-12 12:56:46 -0800

    Add GraphicsEnv to libgui
    This singleton class stores information provided by the framework and
    makes it available to EGL/GLES/Vulkan loaders. It will eventually
    replace vulkan::LoaderData and possibly the egl blob cache directory
    storage.
    
    Bug: 33531483
    Change-Id: I40c3952374badabea738a34a51db90bf197979b8
    Merged-In: If8b84fd38428f1cc9fcee8ffc25eb90cd87cedb4

--- libs/gui/Android.mk
+++ libs/gui/Android.mk
@@ -54,6 +54,7 @@ LOCAL_SRC_FILES := \
 	DisplayEventReceiver.cpp \
 	GLConsumer.cpp \
 	GraphicBufferAlloc.cpp \
+	GraphicsEnv.cpp \
 	GuiConfig.cpp \
 	IDisplayEventConnection.cpp \
 	IGraphicBufferAlloc.cpp \
