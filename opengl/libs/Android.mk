980803a0807c429e2e113ee5c1dad907e6a7ecd4
Mathias Agopian <mathias@google.com>
2017-02-07 13:04:44 -0800

    Remove BoardConfig variables - BOARD_EGL_CFG
    Bug: 34693617
    Test: compiled and booted a device
    Change-Id: I9fcd5e4aa5e68dff2065bd7bffa1950b403a9185

--- opengl/libs/Android.mk
+++ opengl/libs/Android.mk
@@ -1,12 +1,1 @@
 LOCAL_PATH:= $(call my-dir)
-
-# OpenGL drivers config file
-ifneq ($(BOARD_EGL_CFG),)
-include $(CLEAR_VARS)
-LOCAL_MODULE := egl.cfg
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/egl
-LOCAL_SRC_FILES := ../../../../$(BOARD_EGL_CFG)
-include $(BUILD_PREBUILT)
-endif
