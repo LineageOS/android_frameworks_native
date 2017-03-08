littleGnAl/mirror_platform_frameworks_native

test-hwc2: set layer buffer
Test: Ensure "TARGET_USES_HWC2 := true" is in the device's BoardConfig.mk.
      Recompile.
      Run "mm" in frameworks/native/services/surfaceflinger/tests/hwc2.
      Push test-hwc2 to device.
      Run "adb root && adb shell stop".
      Run test case. Ex: "./test-hwc2"

Change-Id: Ie42d1fd4c2d0248c4a3ecf09a9b5871d501f6172

services/surfaceflinger/tests/hwc2/Android.mk
@@ -24,13 +24,27 @@ LOCAL_CFLAGS += \
     -g \
     -Wall -Wextra \
     -Werror \
-    -fno-builtin
-LOCAL_SHARED_LIBRARIES := libcutils libutils libhardware liblog
-LOCAL_STATIC_LIBRARIES := libbase libadf libadfhwc
+    -fno-builtin \
+    -DEGL_EGLEXT_PROTOTYPES \
+    -DGL_GLEXT_PROTOTYPES
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libutils \
+    libhardware \
+    libEGL \
+    libGLESv2 \
+    libui \
+    libgui \
+    liblog
+LOCAL_STATIC_LIBRARIES := \
+    libbase \
+    libadf \
+    libadfhwc
 LOCAL_SRC_FILES := \
     hwc2_test.cpp \
     hwc2_test_properties.cpp \
     hwc2_test_layer.cpp \
-    hwc2_test_layers.cpp
+    hwc2_test_layers.cpp \
+    hwc2_test_buffer.cpp
 
 include $(BUILD_NATIVE_TEST)

LineageOS/android_frameworks_native

Test: Add "#define HAVE_NO_SURFACE_FLINGER" to
          frameworks/native/libs/gui/BufferQueueCore.cpp.
      Recompile and flash.

services/surfaceflinger/tests/hwc2/Android.mk
@@ -24,13 +24,27 @@ LOCAL_CFLAGS += \
     -g \
     -Wall -Wextra \
     -Werror \
-    -fno-builtin
-LOCAL_SHARED_LIBRARIES := libcutils libutils libhardware liblog
-LOCAL_STATIC_LIBRARIES := libbase libadf libadfhwc
+    -fno-builtin \
+    -DEGL_EGLEXT_PROTOTYPES \
+    -DGL_GLEXT_PROTOTYPES
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libutils \
+    libhardware \
+    libEGL \
+    libGLESv2 \
+    libui \
+    libgui \
+    liblog
+LOCAL_STATIC_LIBRARIES := \
+    libbase \
+    libadf \
+    libadfhwc
 LOCAL_SRC_FILES := \
     Hwc2Test.cpp \
     Hwc2TestProperties.cpp \
     Hwc2TestLayer.cpp \
-    Hwc2TestLayers.cpp
+    Hwc2TestLayers.cpp \
+    Hwc2TestBuffer.cpp
 
 include $(BUILD_NATIVE_TEST)