littleGnAl/mirror_platform_frameworks_native

test-hwc2: open and close hwc2 device
Test: Ensure "TARGET_USES_HWC2 := true" is in the device's BoardConfig.mk.
      Recompile.
      Run "mm" in frameworks/native/services/surfaceflinger/tests/hwc2.
      Push test-hwc2 to device.
      Run "adb root && adb shell stop".
      Run test case. Ex: "./test-hwc2"

Change-Id: Ie7d3963540b0c96db8251bd745f37beab8dd340b

services/surfaceflinger/tests/hwc2/Android.mk
@@ -1,0 +1,32 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := test-hwc2
+LOCAL_MODULE_TAGS := tests
+LOCAL_CFLAGS += \
+    -fstack-protector-all \
+    -g \
+    -Wall -Wextra \
+    -Werror \
+    -fno-builtin
+LOCAL_SHARED_LIBRARIES := libcutils libutils libhardware
+LOCAL_STATIC_LIBRARIES := libbase libadf libadfhwc
+LOCAL_SRC_FILES := hwc2_test.cpp
+
+include $(BUILD_NATIVE_TEST)

LineageOS/android_frameworks_native

Test: Add "#define HAVE_NO_SURFACE_FLINGER" to
          frameworks/native/libs/gui/BufferQueueCore.cpp.
      Recompile and flash.

services/surfaceflinger/tests/hwc2/Android.mk
@@ -1,0 +1,32 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := test-hwc2
+LOCAL_MODULE_TAGS := tests
+LOCAL_CFLAGS += \
+    -fstack-protector-all \
+    -g \
+    -Wall -Wextra \
+    -Werror \
+    -fno-builtin
+LOCAL_SHARED_LIBRARIES := libcutils libutils libhardware
+LOCAL_STATIC_LIBRARIES := libbase libadf libadfhwc
+LOCAL_SRC_FILES := Hwc2Test.cpp
+
+include $(BUILD_NATIVE_TEST)