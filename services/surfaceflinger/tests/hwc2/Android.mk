littleGnAl/mirror_platform_frameworks_native

test-hwc2: set layer z order
Test: Ensure "TARGET_USES_HWC2 := true" is in the device's BoardConfig.mk.
      Recompile.
      Run "mm" in frameworks/native/services/surfaceflinger/tests/hwc2.
      Push test-hwc2 to device.
      Run "adb root && adb shell stop".
      Run test case. Ex: "./test-hwc2"

Change-Id: I2c51df36d62f9d809583003b81c24dbd7a744301

services/surfaceflinger/tests/hwc2/Android.mk
@@ -30,6 +30,7 @@ LOCAL_STATIC_LIBRARIES := libbase libadf libadfhwc
 LOCAL_SRC_FILES := \
     hwc2_test.cpp \
     hwc2_test_properties.cpp \
-    hwc2_test_layer.cpp
+    hwc2_test_layer.cpp \
+    hwc2_test_layers.cpp
 
 include $(BUILD_NATIVE_TEST)

LineageOS/android_frameworks_native

Test: Add "#define HAVE_NO_SURFACE_FLINGER" to
          frameworks/native/libs/gui/BufferQueueCore.cpp.
      Recompile and flash.

services/surfaceflinger/tests/hwc2/Android.mk
@@ -30,6 +30,7 @@ LOCAL_STATIC_LIBRARIES := libbase libadf libadfhwc
 LOCAL_SRC_FILES := \
     Hwc2Test.cpp \
     Hwc2TestProperties.cpp \
-    Hwc2TestLayer.cpp
+    Hwc2TestLayer.cpp \
+    Hwc2TestLayers.cpp
 
 include $(BUILD_NATIVE_TEST)