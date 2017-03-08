littleGnAl/mirror_platform_frameworks_native

test-hwc2: set layer composition type
Test: Ensure "TARGET_USES_HWC2 := true" is in the device's BoardConfig.mk.
      Recompile.
      Run "mm" in frameworks/native/services/surfaceflinger/tests/hwc2.
      Push test-hwc2 to device.
      Run "adb root && adb shell stop".
      Run test case. Ex: "./test-hwc2"

Change-Id: I9e71d0c01daa70f28031dd8310f2a40d7162a68e

services/surfaceflinger/tests/hwc2/Android.mk
@@ -27,6 +27,9 @@ LOCAL_CFLAGS += \
     -fno-builtin
 LOCAL_SHARED_LIBRARIES := libcutils libutils libhardware
 LOCAL_STATIC_LIBRARIES := libbase libadf libadfhwc
-LOCAL_SRC_FILES := hwc2_test.cpp
+LOCAL_SRC_FILES := \
+    hwc2_test.cpp \
+    hwc2_test_properties.cpp \
+    hwc2_test_layer.cpp
 
 include $(BUILD_NATIVE_TEST)

LineageOS/android_frameworks_native

Test: Add "#define HAVE_NO_SURFACE_FLINGER" to
          frameworks/native/libs/gui/BufferQueueCore.cpp.
      Recompile and flash.

services/surfaceflinger/tests/hwc2/Android.mk
@@ -27,6 +27,9 @@ LOCAL_CFLAGS += \
     -fno-builtin
 LOCAL_SHARED_LIBRARIES := libcutils libutils libhardware
 LOCAL_STATIC_LIBRARIES := libbase libadf libadfhwc
-LOCAL_SRC_FILES := Hwc2Test.cpp
+LOCAL_SRC_FILES := \
+    Hwc2Test.cpp \
+    Hwc2TestProperties.cpp \
+    Hwc2TestLayer.cpp
 
 include $(BUILD_NATIVE_TEST)