cb4751c52a5625a22b6a11b4de537ff026d9bfe3
Jiwen 'Steve' Cai <jwcai@google.com>
2017-04-14 17:56:55 -0700

    Fix minor corner cases in BufferHubQueueProducer
    1/ Set reasonable return value BufferHubQueueProducer::query.
    2/ Don't error out for setSharedBufferMode(false),
    setAutoRefresh(false), as they are just setting default values again.
    3/ Supports addition buffer metadata: transformt, crop,
    data_format (a.k.a. color_format).
    4/ This also changes BufferHubQueueProducer to be based of
    BnInterface<IGraphicBufferProducer> so that itself can be passed via Binder.
    
    Bug: 37342387
    Bug: 36907193
    Test: exoplayer_demo
    Change-Id: Ie00bb79f6a249e09905ae52f85a85a67744cc90d

--- libs/vr/libbufferhubqueue/include/private/dvr/buffer_hub_queue_core.h
+++ libs/vr/libbufferhubqueue/include/private/dvr/buffer_hub_queue_core.h
@@ -19,16 +19,54 @@ class BufferHubQueueCore {
  public:
   static constexpr int kNoConnectedApi = -1;
 
+  // TODO(b/36187402) The actual implementation of BufferHubQueue's consumer
+  // side logic doesn't limit the number of buffer it can acquire
+  // simultaneously. We need a way for consumer logic to configure and enforce
+  // that.
+  static constexpr int kDefaultUndequeuedBuffers = 1;
+
   // Create a BufferHubQueueCore instance by creating a new producer queue.
   static std::shared_ptr<BufferHubQueueCore> Create();
 
-  // Create a BufferHubQueueCore instance by importing an existing prodcuer queue.
+  // Create a BufferHubQueueCore instance by importing an existing prodcuer
+  // queue.
   static std::shared_ptr<BufferHubQueueCore> Create(
       const std::shared_ptr<ProducerQueue>& producer);
 
-  struct BufferMetadata {
+  // The buffer metadata that an Android Surface (a.k.a. ANativeWindow)
+  // will populate. This must be aligned with the |DvrNativeBufferMetadata|
+  // defined in |dvr_buffer_queue.h|. Please do not remove, modify, or reorder
+  // existing data members. If new fields need to be added, please take extra
+  // care to make sure that new data field is padded properly the size of the
+  // struct stays same.
+  // TODO(b/37578558) Move |dvr_api.h| into a header library so that this
+  // structure won't be copied between |dvr_api.h| and |buffer_hub_qeue_core.h|.
+  struct NativeBufferMetadata {
     // Timestamp of the frame.
     int64_t timestamp;
+
+    // Whether the buffer is using auto timestamp.
+    int32_t is_auto_timestamp;
+
+    // Must be one of the HAL_DATASPACE_XXX value defined in system/graphics.h
+    int32_t dataspace;
+
+    // Crop extracted from an ACrop or android::Crop object.
+    int32_t crop_left;
+    int32_t crop_top;
+    int32_t crop_right;
+    int32_t crop_bottom;
+
+    // Must be one of the NATIVE_WINDOW_SCALING_MODE_XXX value defined in
+    // system/window.h.
+    int32_t scaling_mode;
+
+    // Must be one of the ANATIVEWINDOW_TRANSFORM_XXX value defined in
+    // android/native_window.h
+    int32_t transform;
+
+    // Reserved bytes for so that the struct is forward compatible.
+    int32_t reserved[16];
   };
 
   class NativeBuffer
