5e5459ae9beca8b32e15b97664a4f4b72f1267f0
Shayan Javed <sjaved@nvidia.com>
2017-06-18 20:45:59 -0700

    BlobCache: Use unordered_map instead of vector
    Revert "Revert "egl_cache: Fix get/set wait while saving to disk""
    
    This reverts commit 23bcd7cc36f880d92039b3f5c3fa0e9f12728786.
    
    -Add Blob.h and Blob.cpp
    -Modify BlobCache.h and BlobCache.cpp to use
     std::unordered_map instead of std::vector
    -Set CACHE_*_SIZE defines in Android.bp instead of BoardConfig.mk
    -Use a pthread deferredSaveThread to save to disk
    
    The libutils Blob/BlobCache changes are not needed anymore.
    
    Bug 1941988
    
    Change-Id: I021eda028d4420373bee7c0aa3c63e6aac35b8fa
    Reviewed-on: http://git-psac/r/10661
    Reviewed-by: Jonathan Mccaffrey <jmccaffrey@nvidia.com>
    Reviewed-by: Shayan Javed <sjaved@nvidia.com>
    Tested-by: Shayan Javed <sjaved@nvidia.com>
    Reviewed-by: Robert Tray <rtray@nvidia.com>
    Reviewed-on: http://git-psac.nvidia.com/r/11876
    Reviewed-by: Niranjan Artal <nartal@nvidia.com>
    Tested-by: Niranjan Artal <nartal@nvidia.com>
    Reviewed-on: http://git-psac.nvidia.com/r/12825
    Reviewed-on: https://git-master.nvidia.com/r/1552401
    Reviewed-on: https://git-master.nvidia.com/r/1584983

--- opengl/libs/EGL/egl_cache.cpp
+++ opengl/libs/EGL/egl_cache.cpp
@@ -1,5 +1,5 @@
 /*
- ** Copyright 2011, The Android Open Source Project
+ ** Copyright 2011-2017, The Android Open Source Project
  **
  ** Licensed under the Apache License, Version 2.0 (the "License");
  ** you may not use this file except in compliance with the License.
@@ -28,21 +28,35 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include <memory>
 #include <thread>
+#include <utility>
 
 #include <log/log.h>
 
-// Cache size limits.
-static const size_t maxKeySize = 12 * 1024;
-static const size_t maxValueSize = 64 * 1024;
-static const size_t maxTotalSize = 2 * 1024 * 1024;
+#ifndef MAX_EGL_CACHE_KEY_SIZE
+#define MAX_EGL_CACHE_KEY_SIZE (12 * 1024)
+#endif
+
+#ifndef MAX_EGL_CACHE_ENTRY_SIZE
+#define MAX_EGL_CACHE_ENTRY_SIZE (64 * 1024)
+#endif
+
+#ifndef MAX_EGL_CACHE_SIZE
+#define MAX_EGL_CACHE_SIZE (2 * 1024 * 1024)
+#endif
+
+static const size_t maxKeySize = MAX_EGL_CACHE_KEY_SIZE;
+static const size_t maxValueSize = MAX_EGL_CACHE_ENTRY_SIZE;
+static const size_t maxTotalSize = MAX_EGL_CACHE_SIZE;
 
 // Cache file header
 static const char* cacheFileMagic = "EGL$";
 static const size_t cacheFileHeaderSize = 8;
 
-// The time in seconds to wait before saving newly inserted cache entries.
-static const unsigned int deferredSaveDelay = 4;
+// HashTable initial size
+// TODO: currently unused for mPendingWrites
+static const size_t hashTableInitSize = 400;
 
 // ----------------------------------------------------------------------------
 namespace android {
@@ -72,7 +86,9 @@ static EGLsizeiANDROID getBlob(const void* key, EGLsizeiANDROID keySize,
 // egl_cache_t definition
 //
 egl_cache_t::egl_cache_t() :
-        mInitialized(false) {
+        mInitialized(false),
+        READ_ONLY(false),
+        mYieldBlobCache(false) {
 }
 
 egl_cache_t::~egl_cache_t() {
@@ -123,52 +139,131 @@ void egl_cache_t::initialize(egl_display_t *display) {
 }
 
 void egl_cache_t::terminate() {
+    // Make sure the deferredSaveThread finishes before we destroy mBlobCache
+    saveThreadCheckMutex.lock();
+    if (isSaveThreadRunning) {
+        saveThreadCheckMutex.unlock();
+        // signal thread to exit
+        saveThreadExitMutex.lock();
+        saveThreadExit = true;
+        saveThreadExitMutex.unlock();
+        // wait for thread to finish
+        mCondition.notify_one();
+        pthread_join(deferredSaveThread, NULL);
+    } else {
+        saveThreadCheckMutex.unlock();
+    }
+
     std::lock_guard<std::mutex> lock(mMutex);
+
+    readOnlyMutex.lock();
+    READ_ONLY = true;
+    readOnlyMutex.unlock();
+
     saveBlobCacheLocked();
-    mBlobCache = NULL;
+
+    readOnlyMutex.lock();
+    READ_ONLY = false;
+    readOnlyMutex.unlock();
+
+    // clear the pendingWritesMutex
+    // this shouldn't ideally be needed since
+    // the deferredSaveThread should clear it
+    mPendingWritesMutex.lock();
+    mPendingWrites.clear();
+    mPendingWritesMutex.unlock();
+
+    mBlobCache = nullptr;
 }
 
 void egl_cache_t::setBlob(const void* key, EGLsizeiANDROID keySize,
         const void* value, EGLsizeiANDROID valueSize) {
-    std::lock_guard<std::mutex> lock(mMutex);
 
     if (keySize < 0 || valueSize < 0) {
         ALOGW("EGL_ANDROID_blob_cache set: negative sizes are not allowed");
         return;
     }
 
-    if (mInitialized) {
-        BlobCache* bc = getBlobCacheLocked();
-        bc->set(key, keySize, value, valueSize);
+    if (!mInitialized) {
+        return;
+    }
 
-        if (!mSavePending) {
-            mSavePending = true;
-            std::thread deferredSaveThread([this]() {
-                sleep(deferredSaveDelay);
-                std::lock_guard<std::mutex> lock(mMutex);
-                if (mInitialized) {
-                    saveBlobCacheLocked();
-                }
-                mSavePending = false;
-            });
-            deferredSaveThread.detach();
+    mPendingWritesMutex.lock();
+    std::shared_ptr<Blob> blobKey = std::make_shared<Blob>(key, keySize, true);
+    std::shared_ptr<Blob> blobValue= std::make_shared<Blob>(value, valueSize, true);
+    blobEntry entry(blobKey, blobValue);
+
+    mPendingWrites[blobKey] = entry;
+    mPendingWritesMutex.unlock();
+
+    // start thread to write to BlobCache and then to disk
+    // make sure there is only one thread running at a time
+    std::unique_lock<std::mutex> lock(saveThreadCheckMutex);
+    if (!isSaveThreadRunning) {
+        int rt;
+        rt = pthread_create(&deferredSaveThread, NULL, saveThread, NULL);
+        if (rt) {
+            ALOGW("EGL_ANDROID_blob_cache set: could not create %s thread", deferredSaveThreadName);
+            return;
         }
+
+        rt = pthread_setname_np(deferredSaveThread, deferredSaveThreadName);
+        if (rt) {
+            ALOGW("EGL_ANDROID_blob_cache set: could not name %s thread", deferredSaveThreadName);
+        }
+        isSaveThreadRunning = true;
     }
 }
 
 EGLsizeiANDROID egl_cache_t::getBlob(const void* key, EGLsizeiANDROID keySize,
         void* value, EGLsizeiANDROID valueSize) {
-    std::lock_guard<std::mutex> lock(mMutex);
 
     if (keySize < 0 || valueSize < 0) {
         ALOGW("EGL_ANDROID_blob_cache set: negative sizes are not allowed");
         return 0;
     }
 
+    if (!mInitialized) {
+        return 0;
+    }
+
+    // Check hashtable first
+    mPendingWritesMutex.lock();
+    if (mPendingWrites.size()) {
+        std::shared_ptr<Blob> dummyKey = std::make_shared<Blob>(key, keySize, false);
+        std::unordered_map<std::shared_ptr<Blob>, blobEntry>::const_iterator found = mPendingWrites.find (dummyKey);
+        if (found != mPendingWrites.end()) {
+            blobEntry entry = found->second;
+            std::shared_ptr<Blob> valueBlob = entry.getValue();
+            ssize_t valueBlobSize = valueBlob->getSize();
+            if (valueBlobSize <= valueSize) {
+                memcpy(value, valueBlob->getData(), valueBlobSize);
+            }
+
+            mPendingWritesMutex.unlock();
+            return valueBlobSize;
+        }
+    }
+    mPendingWritesMutex.unlock();
+
+    // Go ahead if read-only, otherwise get a lock on the whole blobcache
+    mYieldBlobCache = true;
+    std::unique_lock<std::mutex> lock(readOnlyMutex);
+
+    if (!READ_ONLY) {
+        lock = std::unique_lock<std::mutex>(mMutex, std::defer_lock);
+        lock.lock();
+    }
+
     if (mInitialized) {
         BlobCache* bc = getBlobCacheLocked();
-        return bc->get(key, keySize, value, valueSize);
+        ssize_t size = bc->get(key, keySize, value, valueSize);
+
+        mYieldBlobCache = false;
+        return size;
     }
+
+    mYieldBlobCache = false;
     return 0;
 }
 
@@ -231,7 +326,7 @@ void egl_cache_t::saveBlobCacheLocked() {
 
         size_t fileSize = headerSize + cacheSize;
 
-        uint8_t* buf = new uint8_t [fileSize];
+        uint8_t* buf = new (std::nothrow) uint8_t [fileSize];
         if (!buf) {
             ALOGE("error allocating buffer for cache contents: %s (%d)",
                     strerror(errno), errno);
