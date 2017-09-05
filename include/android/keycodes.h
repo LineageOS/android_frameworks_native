f84718c3ba8ac73c4c3c16262bca6beba00ee829
Sean hoyt <seanhoyt963@gmail.com>
2017-09-05 12:26:54 +0000

    Add KEY_SOFT_WAKE
    Change-Id: Icd1b4d5380890d3d4c4aa44743c02f9172b6668d

--- include/android/keycodes.h
+++ include/android/keycodes.h
@@ -738,6 +738,7 @@ enum {
     AKEYCODE_MEDIA_STEP_BACKWARD = 275,
     /** Put device to sleep unless a wakelock is held. */
     AKEYCODE_SOFT_SLEEP = 276
+    AKEYCODE_SOFT_WAKE = 277
 
     // NOTE: If you add a new keycode here you must also add it to several other files.
     //       Refer to frameworks/base/core/java/android/view/KeyEvent.java for the full list.
