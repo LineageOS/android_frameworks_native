a33f4b7f16b47c137d5fcf30f45fed6629eea7ea
Siarhei Vishniakou <svv@google.com>
2017-11-06 21:16:47 -0800

    Fix resampling for multiple pointers
    If more than one pointer is present and identical coordinates are
    encountered for any pointer, the lastResample value will not be
    invalidated (by clearing its bits). As a result, the same lastResample
    value will be used to subsequently rewriteMessage any time in the
    future. This can cause significant jumps in the pointer coordinates, and
    is a bug. To further clarify, this bug has only to do with resampling
    and nothing to do with both pointers having the same X or Y value.
    
    This CL makes this logic to be per-pointer. As soon as non-identical
    value is encountered and the timing conditions are such that the
    resampled value is not needed to be used, the lastResample bit for that
    pointer will be cleared, meaning that the value in lastResample for this
    pointer is stale. This value will no longer be used anywhere.
    
    When performing resampling, allow the process to happen on a per-pointer
    basis. If one of the pointers has encountered events with identical
    coordinates, then use the previously resampled value (do not resample
    again), if that value is still valid (see above). Otherwise, the normal
    resampling path will be taken. On the other pointers that do not have
    identical coordinates, go through the normal path as well.
    
    Bug: 68840121
    Test: recorded a repro event with inputstudio and replayed it while
    observing the screen. Saw that the coordinates jump before the fix,
    and do not jump with the fix.
    
    Change-Id: If43c323759de8f0184b76221d1ae599a75349ce9
    Merged-In: If43c323759de8f0184b76221d1ae599a75349ce9
    (cherry picked from commit 3bb597138bc53416b3049caa48c618075ec24231)

--- include/input/InputTransport.h
+++ include/input/InputTransport.h
@@ -381,6 +381,18 @@ private:
             }
         }
 
+        void initializeFrom(const History& other) {
+            eventTime = other.eventTime;
+            idBits = other.idBits; // temporary copy
+            for (size_t i = 0; i < other.idBits.count(); i++) {
+                uint32_t id = idBits.clearFirstMarkedBit();
+                int32_t index = other.idToIndex[id];
+                idToIndex[id] = index;
+                pointers[index].copyFrom(other.pointers[index]);
+            }
+            idBits = other.idBits; // final copy
+        }
+
         const PointerCoords& getPointerById(uint32_t id) const {
             return pointers[idToIndex[id]];
         }
@@ -455,7 +467,6 @@ private:
             int32_t* displayId);
 
     void updateTouchState(InputMessage& msg);
-    bool rewriteMessage(const TouchState& state, InputMessage& msg);
     void resampleTouchState(nsecs_t frameTime, MotionEvent* event,
             const InputMessage *next);
 
@@ -464,6 +475,7 @@ private:
 
     status_t sendUnchainedFinishedSignal(uint32_t seq, bool handled);
 
+    static void rewriteMessage(TouchState& state, InputMessage& msg);
     static void initializeKeyEvent(KeyEvent* event, const InputMessage* msg);
     static void initializeMotionEvent(MotionEvent* event, const InputMessage* msg);
     static void addSample(MotionEvent* event, const InputMessage* msg);
