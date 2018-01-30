e8d3ba646cc53d115f7fd4a74475f7d6e3f5dee3
Chia-I Wu <olv@google.com>
2018-01-30 14:07:34 -0800

    libui: set USAGE_HW_2D in getValid10UsageBits
    Bug: 72703005
    Test: Manual
    
    Change-Id: I48e2c5651881fe6d3b809e978787ffc9a6de9fab
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- libs/ui/Gralloc2.cpp
+++ libs/ui/Gralloc2.cpp
@@ -42,8 +42,8 @@ uint64_t getValid10UsageBits() {
         for (const auto bit : hardware::hidl_enum_iterator<BufferUsage>()) {
             bits = bits | bit;
         }
-        // TODO(b/72323293): Remove this mask for EXTERNAL_DISP.
-        bits = bits | (1 << 13);
+        // TODO(b/72323293, b/72703005): Remove these additional bits
+        bits = bits | (1 << 10) | (1 << 13);
 
         return bits;
     }();
