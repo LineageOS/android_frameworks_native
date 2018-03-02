dd9b2ae8500d85a6aba82de15e8b40d14a921ca6
Peiyong Lin <lpy@google.com>
2018-03-01 16:22:45 -0800

    [SurfaceFinger] Adds auto color mode support in SurfaceFlinger.
    This patch adds auto color mode support in SurfaceFlinger. For Auto Awesome
    Color milestone 1, we expect the hardware composer is capable of handling P3
    layers, meaning if the hardware composer is given P3 layers, it will strech the
    color from Display P3 to the pannel native color space. Hardware composer may
    punt sRGB layers back to SurfaceFlinger, in this case, we fall back to
    RenderEngine. We will set the destination data space to Display P3 when the
    layers are mixed color spaces, and set the destination data space as SRGB if
    they are all SRGB layers.
    
    BUG: 73824924
    BUG: 73825729
    Test: Build
    Change-Id: I577841b14de0cfe8c29f8aa30bee8621c5d72976

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -123,6 +123,12 @@ enum {
     eTransactionMask          = 0x0f,
 };
 
+enum class DisplayColorSetting : int32_t {
+    MANAGED = 0,
+    UNMANAGED = 1,
+    ENHANCED = 2,
+};
+
 // A thin interface to abstract creating instances of Surface (gui/Surface.h) to
 // use as a NativeWindow.
 class NativeWindowSurface {
@@ -467,7 +473,9 @@ private:
                               bool stateLockHeld);
 
     // Called on the main thread in response to setActiveColorMode()
-    void setActiveColorModeInternal(const sp<DisplayDevice>& hw, ui::ColorMode colorMode);
+    void setActiveColorModeInternal(const sp<DisplayDevice>& hw,
+                                    ui::ColorMode colorMode,
+                                    ui::Dataspace dataSpace);
 
     // Returns whether the transaction actually modified any state
     bool handleMessageTransaction();
@@ -641,9 +649,10 @@ private:
 
     // Given a dataSpace, returns the appropriate color_mode to use
     // to display that dataSpace.
-    ui::ColorMode pickColorMode(ui::Dataspace dataSpace) const;
-    ui::Dataspace bestTargetDataSpace(ui::Dataspace a, ui::Dataspace b,
-            bool hasHdr) const;
+    ui::Dataspace getBestDataspace(const sp<const DisplayDevice>& displayDevice) const;
+    void pickColorMode(const sp<DisplayDevice>& displayDevice,
+                       ui::ColorMode* outMode,
+                       ui::Dataspace* outDataSpace) const;
 
     mat4 computeSaturationMatrix() const;
 
@@ -846,7 +855,6 @@ private:
 
     size_t mNumLayers;
 
-
     // Verify that transaction is being called by an approved process:
     // either AID_GRAPHICS or AID_SYSTEM.
     status_t CheckTransactCodeCredentials(uint32_t code);
@@ -856,8 +864,12 @@ private:
     static bool useVrFlinger;
     std::thread::id mMainThreadId;
 
-    float mSaturation = 1.0f;
-    bool mForceNativeColorMode = false;
+    DisplayColorSetting mDisplayColorSetting = DisplayColorSetting::MANAGED;
+    // Applied on sRGB layers when the render intent is non-colorimetric.
+    mat4 mLegacySrgbSaturationMatrix;
+    // Applied globally.
+    float mGlobalSaturationFactor = 1.0f;
+    bool mBuiltinDisplaySupportsEnhance = false;
 
     using CreateBufferQueueFunction =
             std::function<void(sp<IGraphicBufferProducer>* /* outProducer */,
