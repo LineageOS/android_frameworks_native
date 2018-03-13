1dc3dfaf8dbb5160062aaa1b0129ef9fe628a70d
Mathieu Chartier <mathieuc@google.com>
2018-03-12 17:55:06 -0700

    Add flag for controling app image generation
    So that package manager service can control this.
    
    Bug: 72696798
    Test: manual
    
    (cherry picked from commit ad45a1b89bc3b6ec84ac32c4200be3d3c1d056fa)
    
    Merged-In: I15fa74aff24ef14d22541117408840f80b7d300f
    Change-Id: If8886187a67f392dd83061b9f44fae8d3bac22be

--- cmds/installd/tests/installd_otapreopt_test.cpp
+++ cmds/installd/tests/installd_otapreopt_test.cpp
@@ -113,6 +113,7 @@ protected:
             case 6: return "6";
             case 7: return "7";
             case 8: return "8";
+            case 9: return "9";
         }
         return nullptr;
     }
