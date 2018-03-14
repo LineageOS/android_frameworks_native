2a10e6094d7ff215c5939a39da8b25664d79e59b
Saurabh Dubey <sdubey@codeaurora.org>
2018-03-14 16:22:57 +0530

    SF: Add support for 64 bit consumer usage flag
    Change consumer usage query calls to get 64 bit flags.
    Make modifications to accomodate 64 bit flags.
    
    Change-Id: I55c0811cb58dca791ed1b3a70d2de876aeac042c
    CRs-Fixed: 2223978

--- services/surfaceflinger/ExSurfaceFlinger/ExVirtualDisplaySurface.cpp
+++ services/surfaceflinger/ExSurfaceFlinger/ExVirtualDisplaySurface.cpp
@@ -47,12 +47,12 @@ ExVirtualDisplaySurface::ExVirtualDisplaySurface(HWComposer& hwc, int32_t dispId
         bool secure)
 :   VirtualDisplaySurface(hwc, dispId, sink, bqProducer, bqConsumer, name),
    mSecure(secure) {
-   sink->query(NATIVE_WINDOW_CONSUMER_USAGE_BITS, &mSinkUsage);
+   sink->getConsumerUsage(&mSinkUsage);
    mSinkUsage |= GRALLOC_USAGE_HW_COMPOSER;
    setOutputUsage(mSinkUsage);
 
     if (HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED == mOutputFormat) {
-        int sinkUsage = (mSinkUsage & GRALLOC_USAGE_HW_VIDEO_ENCODER);
+        uint64_t sinkUsage = (mSinkUsage & GRALLOC_USAGE_HW_VIDEO_ENCODER);
         sinkUsage |= (mSinkUsage & GRALLOC_USAGE_PRIVATE_WFD);
 
         if (!sinkUsage) {
