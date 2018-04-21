5c397d3fdfddcb98835125af35846f08858b141c
Aaron Kling <webgeek1234@gmail.com>
2018-04-20 22:12:29 -0500

    Fix eglMakeCurrent crash when in opengl contexts
    Nvidia shield devices support using egl to switch to a full desktop
    opengl context. In opengl 3.0+, GL_EXTENSIONS have to be retrieved
    with glGetStringi and is invalid for glGetString. Thus,
    eglMakecurrent can crash with a NPE if this case is not handled.
    The logic here is building a wrapper for glGetStringi, thus the
    error can be ignored. This patch allows GL_EXT_debug_marker to
    still pushed into the tokenized extension string used by the
    glGetStringi wrapper.
    
    Change-Id: I9c599e10c62aabf684bde4e81719aa248327ac80

--- opengl/libs/EGL/egl_object.cpp
+++ opengl/libs/EGL/egl_object.cpp
@@ -319,6 +319,14 @@ void egl_context_t::onMakeCurrent(EGLSurface draw, EGLSurface read) {
     const char* exts = (const char*)gEGLImpl.hooks[version]->gl.glGetString(GL_EXTENSIONS);
     if (!exts) return;
 
+#ifdef NV_ANDROID_FRAMEWORK_ENHANCEMENTS
+    // In an opengl 3.0+ context, GL_EXTENSIONS is not valid for glGetString
+    if (exts == NULL) {
+        gEGLImpl.hooks[version]->gl.glGetError();
+        exts = "";
+    }
+#endif
+
     // If this context is sharing with another context, and the other context was reset
     // e.g. due to robustness failure, this context might also be reset and glGetString can
     // return NULL.
