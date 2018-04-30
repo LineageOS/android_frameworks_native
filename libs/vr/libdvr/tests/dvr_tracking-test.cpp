e7ff66349949768ad90e63376f7e4f2e9ad14760
Jiwen 'Steve' Cai <jwcai@google.com>
2018-04-30 11:10:12 -0700

    Implement basic dvr_tracking-test
    This test is relative simple as it has great overlap with the Android
    native test libdvrtracking-test; and Google3 side of DVR Tracking API
    test.
    
    Bug: 78805053
    Bug: 78173557
    Test: dvr_tracking-test
    Change-Id: I148219aabfcb2c684f7dce017b4f4e4fe636c2ab

--- libs/vr/libdvr/tests/dvr_tracking-test.cpp
+++ libs/vr/libdvr/tests/dvr_tracking-test.cpp
@@ -8,8 +8,34 @@ namespace {
 class DvrTrackingTest : public DvrApiTest {};
 
 #if DVR_TRACKING_IMPLEMENTED
-// TODO(b/78173557): Implement dvrTrackingXXX API test.
-#else
+
+TEST_F(DvrTrackingTest, Implemented) {
+  ASSERT_TRUE(api_.TrackingCameraCreate != nullptr);
+  ASSERT_TRUE(api_.TrackingCameraStart != nullptr);
+  ASSERT_TRUE(api_.TrackingCameraStop != nullptr);
+}
+
+TEST_F(DvrTrackingTest, CreateFailsForInvalidInput) {
+  int ret;
+  ret = api_.TrackingCameraCreate(nullptr);
+  EXPECT_EQ(ret, -EINVAL);
+
+  DvrTrackingCamera* camera = reinterpret_cast<DvrTrackingCamera*>(42);
+  ret = api_.TrackingCameraCreate(&camera);
+  EXPECT_EQ(ret, -EINVAL);
+}
+
+TEST_F(DvrTrackingTest, CreateDestroy) {
+  DvrTrackingCamera* camera = nullptr;
+  int ret = api_.TrackingCameraCreate(&camera);
+
+  EXPECT_EQ(ret, 0);
+  ASSERT_TRUE(camera != nullptr);
+
+  api_.TrackingCameraDestroy(camera);
+}
+
+#else  // !DVR_TRACKING_IMPLEMENTED
 
 TEST_F(DvrTrackingTest, NotImplemented) {
   ASSERT_TRUE(api_.TrackingCameraCreate != nullptr);
