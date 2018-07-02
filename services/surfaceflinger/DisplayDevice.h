b0fb87e8f20fdc9ce9c5e5bfddea37da22ea70e3
LuK1337 <priv.luk@gmail.com>
2018-07-02 10:31:21 +0200

    Revert "SF: Squashed hwrotation support"
    This reverts commit a9fa1c9c19d6ac00aa0073ef23b7a1d95f8e0a90.
    
    Change-Id: Icb9eb3e733f6a40acf6233adb11216914f5a3db5

--- services/surfaceflinger/DisplayDevice.h
+++ services/surfaceflinger/DisplayDevice.h
@@ -245,7 +245,7 @@ private:
     /*
      * Transaction state
      */
-    status_t orientationToTransfrom(int orientation,
+    static status_t orientationToTransfrom(int orientation,
             int w, int h, Transform* tr);
 
     // The identifier of the active layer stack for this display. Several displays
@@ -267,8 +267,6 @@ private:
     int mPowerMode;
     // Current active config
     int mActiveConfig;
-    // Panel hardware rotation
-    int32_t mHardwareRotation;
 #ifdef USE_HWC2
     // current active color mode
     android_color_mode_t mActiveColorMode;
