littleGnAl/mirror_platform_frameworks_native

Need GSI to support landscape LCM
When device uses landscape LCM, the nature of the screen shows that
the device is a portrait device. After we flash with GSI, there is
vendor solution for landscape LCM used at portrait device. As the
result, the screen displays landscape layout and orientation while
device is at portrait orientation. In addition, the sensor coordinate
system mismatches with android coordinate system.

We suggest Google can add config to handle the case "portrait device
uses landscape LCM or landscape device uses portrait LCM".

Bug: 69691076

Test: We verified following test cases for this patch.
1. Make sure homescreen is normal
2. Rotate device and make sure screen is normal
3. Grab screen shot and check if screen shot is normal
4. Connect to Wi-Fi display and make sure WFD screen is normal

Change-Id: I6675e2a39b03c99ae9fab145fde3cd4eda639564

services/surfaceflinger/DisplayDevice.cpp
@@ -582,6 +582,15 @@ void DisplayDevice::setProjection(int orientation,
     TL.set(-src_x, -src_y);
     TP.set(dst_x, dst_y);
 
+    // need to take care of HW rotation for mGlobalTransform
+    // for case if the panel is not installed align with device orientation
+    if (mType == DisplayType::DISPLAY_PRIMARY) {
+        int hworient = mFlinger->getHwOrientation();
+        DisplayDevice::orientationToTransfrom(
+                (orientation + hworient) % (DisplayState::eOrientation270 + 1),
+                w, h, &R);
+    }
+
     // The viewport and frame are both in the logical orientation.
     // Apply the logical translation, scale to physical size, apply the
     // physical translation and finally rotate to the physical orientation.

LineageOS/android_frameworks_native

* This patch adds a reworked implementation of ro.sf.hwrotation instead
  of the LineageOS one, which was actually a forward-port of AOSP that
  had removed support for it.
* Original patch submitted at:
  https://android-review.googlesource.com/c/platform/frameworks/native/+/577262
* Also backported for the libsurfaceflinger implementation that uses
  legacy libhwcomposer (original patch is for the hwc2-compatible
  libsurfaceflinger only).
(cherry picked from 8686db3b5f5b6107a65abba2ab1e1bc4b7e1fe8d)
Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

services/surfaceflinger/DisplayDevice.cpp
@@ -574,6 +574,15 @@ void DisplayDevice::setProjection(int orientation,
     TL.set(-src_x, -src_y);
     TP.set(dst_x, dst_y);
 
+    // need to take care of HW rotation for mGlobalTransform
+    // for case if the panel is not installed align with device orientation
+    if (mType == DisplayType::DISPLAY_PRIMARY) {
+        int hw_orient = mFlinger->getHwOrientation();
+        DisplayDevice::orientationToTransfrom(
+                (orientation + hw_orient) % (DisplayState::eOrientation270 + 1),
+                w, h, &R);
+    }
+
     // The viewport and frame are both in the logical orientation.
     // Apply the logical translation, scale to physical size, apply the
     // physical translation and finally rotate to the physical orientation.