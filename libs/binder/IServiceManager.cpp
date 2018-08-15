3b343794e75a3aa7af15b62d60e295cf664ac838
LuK1337 <priv.luk@gmail.com>
2018-08-15 19:22:49 +0200

    libbinder: Don't log call trace when waiting for vendor service on non-eng builds
    * On some devices this results in a lot of unnecessary log spam.
    
    Change-Id: Id797a2ce2590ebc718e3bf84c4edeb763ffd379d

--- libs/binder/IServiceManager.cpp
+++ libs/binder/IServiceManager.cpp
@@ -157,12 +157,17 @@ public:
         // retry interval in millisecond.
         const long sleepTime = gSystemBootCompleted ? 1000 : 100;
 
+        char buildType[PROPERTY_VALUE_MAX];
+        property_get("ro.build.type", buildType, "unknown");
+        const bool isBuildTypeEng = strcmp(buildType, "eng") == 0;
+
         int n = 0;
         while (uptimeMillis() < timeout) {
             n++;
             if (isVendorService) {
+                property_get("ro.build.type", buildType, "unknown");
                 ALOGI("Waiting for vendor service %s...", String8(name).string());
-                CallStack stack(LOG_TAG);
+                if (isBuildTypeEng) CallStack stack(LOG_TAG);
             } else if (n%10 == 0) {
                 ALOGI("Waiting for service %s...", String8(name).string());
             }
