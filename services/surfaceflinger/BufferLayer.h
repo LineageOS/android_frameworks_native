4d265be030101df2bcdb9ff3b95785f258bd5b59
Mohan Pallaka <mpallaka@codeaurora.org>
2018-09-19 10:12:52 -0700

    sf: Do not black out HDR layer
    Remove blacking out HDR when the device has
    HDR display.
    
    Change-Id: Ib3fe757995fce1fb41d94177e8424876ee423ccf
    CRs-Fixed: 2284433

--- services/surfaceflinger/BufferLayer.h
+++ services/surfaceflinger/BufferLayer.h
@@ -136,6 +136,7 @@ public:
 
     bool isOpaque(const Layer::State& s) const override;
     virtual bool isHDRLayer() const { return false; }
+    virtual bool hasHdrDisplay() const { return false; }
     virtual bool canAllowGPUForProtected() const { return false; }
     virtual bool isScreenshot() const { return false; }
 
