c19c604ba7707294a1751c0d2339c369ec305b32
Roland Levillain <rpl@google.com>
2018-12-18 12:15:12 +0000

    Revert^2 "Have `otapreopt_chroot` mount APEX packages in the chroot dir."
    We need at least the Android Runtime APEX to be able to run `dex2oat`
    in `otapreopt`. For simplicity, reuse `apexd`'s logic and mount all
    the APEX packages found on the system partition in /postinstall/apex.
    
    This is a temporary change. The final design will integrate A/B OTA
    updates and `apexd`.
    
    This reverts commit 00b5b3f01b7b6bf96d0d66d650ede8aa4734bf3f.
    
    Test: m otapreopt_chroot
    Test: A/B OTA update test (asit/dexoptota/self_full).
    Bug: 113373927
    Bug: 120796514
    Change-Id: Ifa45c282d04ae443e55bd41d8c9cbb4ea5f837d4

--- cmds/installd/otapreopt_chroot.cpp
+++ cmds/installd/otapreopt_chroot.cpp
@@ -17,6 +17,7 @@
 #include <fcntl.h>
 #include <linux/unistd.h>
 #include <sys/mount.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 
 #include <sstream>
@@ -24,6 +25,9 @@
 #include <android-base/logging.h>
 #include <android-base/macros.h>
 #include <android-base/stringprintf.h>
+#include <selinux/android.h>
+
+#include <apexd.h>
 
 #include "installd_constants.h"
 #include "otapreopt_utils.h"
@@ -138,6 +142,33 @@ static int otapreopt_chroot(const int argc, char **arg) {
       UNUSED(product_result);
     }
 
+    // Setup APEX mount point and its security context.
+    static constexpr const char* kPostinstallApexDir = "/postinstall/apex";
+    // The following logic is similar to the one in system/core/rootdir/init.rc:
+    //
+    //   mount tmpfs tmpfs /apex nodev noexec nosuid
+    //   chmod 0755 /apex
+    //   chown root root /apex
+    //   restorecon /apex
+    //
+    if (mount("tmpfs", kPostinstallApexDir, "tmpfs", MS_NODEV | MS_NOEXEC | MS_NOSUID, nullptr)
+        != 0) {
+        PLOG(ERROR) << "Failed to mount tmpfs in " << kPostinstallApexDir;
+        exit(209);
+    }
+    if (chmod(kPostinstallApexDir, 0755) != 0) {
+        PLOG(ERROR) << "Failed to chmod " << kPostinstallApexDir << " to 0755";
+        exit(210);
+    }
+    if (chown(kPostinstallApexDir, 0, 0) != 0) {
+        PLOG(ERROR) << "Failed to chown " << kPostinstallApexDir << " to root:root";
+        exit(211);
+    }
+    if (selinux_android_restorecon(kPostinstallApexDir, 0) < 0) {
+        PLOG(ERROR) << "Failed to restorecon " << kPostinstallApexDir;
+        exit(212);
+    }
+
     // Chdir into /postinstall.
     if (chdir("/postinstall") != 0) {
         PLOG(ERROR) << "Unable to chdir into /postinstall.";
@@ -154,6 +185,16 @@ static int otapreopt_chroot(const int argc, char **arg) {
         PLOG(ERROR) << "Unable to chdir into /.";
         exit(205);
     }
+
+    // Try to mount APEX packages in "/apex" in the chroot dir. We need at least
+    // the Android Runtime APEX, as it is required by otapreopt to run dex2oat.
+    // The logic here is (partially) copied and adapted from
+    // system/apex/apexd/apexd_main.cpp.
+    //
+    // Only scan the APEX directory under /system (within the chroot dir).
+    // Note that this leaves around the loop devices created and used by
+    // libapexd's code, but this is fine, as we expect to reboot soon after.
+    apex::scanPackagesDirAndActivate(apex::kApexPackageSystemDir);
 
     // Now go on and run otapreopt.
 
