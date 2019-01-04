05290adee2d8941a112ede58944885d1835976d9
Nandana Dutt <nandana@google.com>
2019-01-04 15:48:09 +0000

    Add more arguments to Dumpstate binder interface
    Add output file descriptors and listener to startBugreport.
    
    Bug: 111441001
    Test: builds
    Change-Id: If42deb3ddc944fc3756025dba49da249dd959212

--- cmds/dumpstate/DumpstateService.h
+++ cmds/dumpstate/DumpstateService.h
@@ -20,6 +20,7 @@
 #include <mutex>
 #include <vector>
 
+#include <android-base/unique_fd.h>
 #include <binder/BinderService.h>
 
 #include "android/os/BnDumpstate.h"
@@ -41,7 +42,9 @@ class DumpstateService : public BinderService<DumpstateService>, public BnDumpst
                                bool getSectionDetails,
                                sp<IDumpstateToken>* returned_token) override;
 
-    binder::Status startBugreport(int fd, int bugreport_mode, int32_t* returned_id) override;
+    binder::Status startBugreport(const android::base::unique_fd& bugreportFd,
+                                  const android::base::unique_fd& screenshotFd, int bugreport_mode,
+                                  const sp<IDumpstateListener>& listener) override;
 
   private:
     Dumpstate& ds_;
