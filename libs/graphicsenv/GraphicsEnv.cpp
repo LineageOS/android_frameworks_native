6a5f97c212c093eefa766471a6e22166707ac0dd
Jakub Pawlowski <jpawlowski@google.com>
2019-01-10 00:25:41 +0000

    Revert "Enable ANGLE-for-Android rule processing again"
    This reverts commit 6d24de491e07a68526bbf760f2d2df2a7a756ea8.
    
    Reason for revert: b/122509524 Random bluetooth crash observed in QP1A.190108.001
    
    Change-Id: Ib10d2b58822ba0895160dda5f4d2093672af091b

--- libs/graphicsenv/GraphicsEnv.cpp
+++ libs/graphicsenv/GraphicsEnv.cpp
@@ -284,7 +284,16 @@ void GraphicsEnv::updateUseAngle() {
     } else {
         // The "Developer Options" value wasn't set to force the use of ANGLE.  Need to temporarily
         // load ANGLE and call the updatable opt-in/out logic:
-        void* featureSo = loadLibrary("feature_support");
+
+        // Check if ANGLE is enabled. Workaround for several bugs:
+        // b/119305693 b/119322355 b/119305887
+        // Something is not working correctly in the feature library
+        char prop[PROPERTY_VALUE_MAX];
+        property_get("debug.angle.enable", prop, "0");
+        void* featureSo = nullptr;
+        if (atoi(prop)) {
+            featureSo = loadLibrary("feature_support");
+        }
         if (featureSo) {
             ALOGV("loaded ANGLE's opt-in/out logic from namespace");
             mUseAngle = checkAngleRules(featureSo);
