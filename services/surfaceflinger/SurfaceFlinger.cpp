9e5d558d00df1b4e8349087718807f632236dea8
Alec Mouri <alecmouri@google.com>
2019-04-02 18:59:31 -0700

    Always set phase offsets in setRefreshRateTo.
    Incremental syncs can cause SF's configuration to be out of sync with
    HWC, which allows for bad phase offsets.
    
    Test: dumpsys
    Change-Id: Ide92a15853ce50cbff8b03e474956b96fb1e5c8b

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -1485,12 +1485,13 @@ void SurfaceFlinger::setRefreshRateTo(RefreshRateType refreshRate, Scheduler::Co
         return;
     }
 
+    mPhaseOffsets->setRefreshRateType(refreshRate);
+
     const auto display = getDisplayDeviceLocked(displayToken);
     if (desiredConfigId == display->getActiveConfig()) {
         return;
     }
 
-    mPhaseOffsets->setRefreshRateType(refreshRate);
     setDesiredActiveConfig({refreshRate, desiredConfigId, getInternalDisplayTokenLocked(), event});
 }
 
