8a8ca15813b6718b1508afd6fdfbd065d455c2ba
Roland Levillain <rpl@google.com>
2019-07-11 18:48:05 +0100

    Check that a Runtime APEX has been activated before running otapreopt.
    Test: m otapreopt_chroot
    Test: A/B OTA update test (asit/dexoptota/self_full).
    Bug: 137255082
    Change-Id: I0f5a6287d2f7df6a1acb667994dc8edd4ed6ed4c

--- cmds/installd/otapreopt_chroot.cpp
+++ cmds/installd/otapreopt_chroot.cpp
@@ -236,6 +236,18 @@ static int otapreopt_chroot(const int argc, char **arg) {
     // the Android Runtime APEX, as it is required by otapreopt to run dex2oat.
     std::vector<apex::ApexFile> active_packages = ActivateApexPackages();
 
+    // Check that an Android Runtime APEX has been activated; clean up and exit
+    // early otherwise.
+    if (std::none_of(active_packages.begin(),
+                     active_packages.end(),
+                     [](const apex::ApexFile& package){
+                         return package.GetManifest().name() == "com.android.runtime";
+                     })) {
+        LOG(FATAL_WITHOUT_ABORT) << "No activated com.android.runtime APEX package.";
+        DeactivateApexPackages(active_packages);
+        exit(217);
+    }
+
     // Now go on and run otapreopt.
 
     // Incoming:  cmd + status-fd + target-slot + cmd...      | Incoming | = argc
