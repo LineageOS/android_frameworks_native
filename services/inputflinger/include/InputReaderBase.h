2d8cc8945eb1c1be5ce6a262613a5aae6e257d06
Pin-Yu Huang <matthuang@google.com>
2019-07-19 04:00:44 +0000

    Revert "Use std::set instead of SortedVector"
    This reverts commit a03f0debad92921ba52d33e7e06f0064703e85ad.
    
    Reason for revert: cause BB
    
    Change-Id: I5531b895947236ff5754334f3cd4660d07513293

--- services/inputflinger/include/InputReaderBase.h
+++ services/inputflinger/include/InputReaderBase.h
@@ -27,11 +27,11 @@
 #include <utils/KeyedVector.h>
 #include <utils/Thread.h>
 #include <utils/RefBase.h>
+#include <utils/SortedVector.h>
 
+#include <optional>
 #include <stddef.h>
 #include <unistd.h>
-#include <optional>
-#include <set>
 #include <unordered_map>
 #include <vector>
 
@@ -250,7 +250,7 @@ struct InputReaderConfiguration {
     bool pointerCapture;
 
     // The set of currently disabled input devices.
-    std::set<int32_t> disabledDevices;
+    SortedVector<int32_t> disabledDevices;
 
     InputReaderConfiguration() :
             virtualKeyQuietTime(0),
