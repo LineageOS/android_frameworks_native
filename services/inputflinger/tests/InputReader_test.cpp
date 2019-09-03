2770d2473085bc0878e56cad5f5a4c75591d2118
Prabir Pradhan <prabirmsp@google.com>
2019-09-02 18:07:11 -0700

    Create inputmappers and accumulators directories
    Inside the 'inputreader' directory, we create an 'mapper' directory
    to hold the InputMappers and a 'mapper/accumulator' directory to
    hold the Accumulators.
    
    Bug: 140139676
    Test: atest inputflinger_tests
    Test: Touch, keyboard, trackpad, and mouse works on crosshatch
    Change-Id: I4966919cd5b42353dd8c1e7cc7663f6c83765542

--- services/inputflinger/tests/InputReader_test.cpp
+++ services/inputflinger/tests/InputReader_test.cpp
@@ -14,16 +14,16 @@
  * limitations under the License.
  */
 
-#include "InputReader.h"
-#include "CursorInputMapper.h"
-#include "InputDevice.h"
-#include "InputMapper.h"
-#include "KeyboardInputMapper.h"
-#include "MultiTouchInputMapper.h"
-#include "SingleTouchInputMapper.h"
-#include "SwitchInputMapper.h"
-#include "TestInputListener.h"
-#include "TouchInputMapper.h"
+#include <CursorInputMapper.h>
+#include <InputDevice.h>
+#include <InputMapper.h>
+#include <InputReader.h>
+#include <KeyboardInputMapper.h>
+#include <MultiTouchInputMapper.h>
+#include <SingleTouchInputMapper.h>
+#include <SwitchInputMapper.h>
+#include <TestInputListener.h>
+#include <TouchInputMapper.h>
 
 #include <gtest/gtest.h>
 #include <inttypes.h>
