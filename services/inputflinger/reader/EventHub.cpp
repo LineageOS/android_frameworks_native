6cad682da2250398ee64c6a7975cde842e7290a4
Aaron Kling <webgeek1234@gmail.com>
2019-11-04 11:39:49 -0600

    Add Nvidia ShieldTech hooks [2/2]
    This patch was released by Nvidia, but without history or
    authorship. It was then modified slightly to be less invasive on
    devices not using the functionality.
    
    Change-Id: Ia7822e2401e56a240baa31977afcf254d9cabeff

--- services/inputflinger/reader/EventHub.cpp
+++ services/inputflinger/reader/EventHub.cpp
@@ -69,6 +69,10 @@ namespace android {
 
 static constexpr bool DEBUG = false;
 
+#ifdef NV_ANDROID_FRAMEWORK_ENHANCEMENTS
+static InputHook* mInputHook = NULL;
+#endif
+
 static const char* DEVICE_PATH = "/dev/input";
 // v4l2 devices go directly into /dev
 static const char* VIDEO_DEVICE_PATH = "/dev";
@@ -204,6 +208,9 @@ EventHub::Device::Device(int fd, int32_t id, const std::string& path,
 }
 
 EventHub::Device::~Device() {
+#ifdef NV_ANDROID_FRAMEWORK_ENHANCEMENTS
+    mInputHook->filterCloseDevice(id);
+#endif
     close();
     delete configuration;
 }
@@ -409,6 +416,9 @@ status_t EventHub::getAbsoluteAxisInfo(int32_t deviceId, int axis,
             }
             return OK;
         }
+#ifdef NV_ANDROID_FRAMEWORK_ENHANCEMENTS
+        return mInputHook->getAbsoluteAxisInfo(deviceId, axis, outAxisInfo);
+#endif
     }
     return -1;
 }
@@ -511,6 +521,9 @@ status_t EventHub::getAbsoluteAxisValue(int32_t deviceId, int32_t axis, int32_t*
             *outValue = info.value;
             return OK;
         }
+#ifdef NV_ANDROID_FRAMEWORK_ENHANCEMENTS
+        return mInputHook->getAbsoluteAxisValue(deviceId, axis, outValue);
+#endif
     }
     return -1;
 }
@@ -991,11 +1004,32 @@ size_t EventHub::getEvents(int timeoutMillis, RawEvent* buffer, size_t bufferSiz
                     ALOGE("could not get event (wrong size: %d)", readSize);
                 } else {
                     int32_t deviceId = device->id == mBuiltInKeyboardId ? 0 : device->id;
+#ifdef NV_ANDROID_FRAMEWORK_ENHANCEMENTS
+                    int32_t oldDeviceId = deviceId;
+#endif
 
                     size_t count = size_t(readSize) / sizeof(struct input_event);
                     for (size_t i = 0; i < count; i++) {
                         struct input_event& iev = readBuffer[i];
                         event->when = processEventTimestamp(iev);
+
+#ifdef NV_ANDROID_FRAMEWORK_ENHANCEMENTS
+                        deviceId = oldDeviceId;
+
+                        InputHook::RESPONSE resp = mInputHook->filterEvent(iev, deviceId);
+                        switch (resp) {
+                        case InputHook::EVENT_DEFAULT:
+                        case InputHook::EVENT_PROCESS:
+                            break;
+                        case InputHook::EVENT_SKIP:
+                            continue;
+                        case InputHook::EVENT_ADD:
+                            // Decrement i so we reprocess this event
+                            --i;
+                            break;
+                        }
+#endif
+
                         event->deviceId = deviceId;
                         event->type = iev.type;
                         event->code = iev.code;
@@ -1122,6 +1156,9 @@ void EventHub::scanDevicesLocked() {
     if (mDevices.indexOfKey(ReservedInputDeviceId::VIRTUAL_KEYBOARD_ID) < 0) {
         createVirtualKeyboardLocked();
     }
+#ifdef NV_ANDROID_FRAMEWORK_ENHANCEMENTS
+    mInputHook->registerDevices();
+#endif
 }
 
 // ----------------------------------------------------------------------------
@@ -1287,6 +1324,16 @@ status_t EventHub::openDeviceLocked(const char* devicePath) {
 
     // Allocate device.  (The device object takes ownership of the fd at this point.)
     int32_t deviceId = mNextDeviceId++;
+
+#ifdef NV_ANDROID_FRAMEWORK_ENHANCEMENTS
+    // Enable input filtering. A return of false means to discard this input
+    if (!mInputHook->filterNewDevice(fd, deviceId, String8(devicePath), identifier)) {
+        ALOGD("Input filtering has rejecting this device");
+        close(fd);
+        return -1;
+    }
+#endif
+    
     Device* device = new Device(fd, deviceId, devicePath, identifier);
 
     ALOGV("add device %d: %s\n", deviceId, devicePath);
