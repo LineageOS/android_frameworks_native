331ea664c89d24657a533aeb16952a47762bbc42
Calin Juravle <calin@google.com>
2020-01-28 16:24:05 -0800

    [installd] Remove boot complete marker
    ART doesn't load the boot image from /data anymore and the marker is
    useless.
    
    Test: device boots
    Bug: 143899228
    
    (cherry picked from commit 9308a5444065a5847769104e81ee361cf060115b)
    
    Merged-In: I78defa45b52b868da9861a3aac28d15d8c453d16
    Change-Id: I1d06767e29c76d2f19d35bd8ae53bc5d27bda810

--- cmds/installd/InstalldNativeService.h
+++ cmds/installd/InstalldNativeService.h
@@ -123,7 +123,6 @@ public:
             int32_t uid);
     binder::Status removeIdmap(const std::string& overlayApkPath);
     binder::Status rmPackageDir(const std::string& packageDir);
-    binder::Status markBootComplete(const std::string& instructionSet);
     binder::Status freeCache(const std::unique_ptr<std::string>& uuid, int64_t targetFreeBytes,
             int64_t cacheReservedBytes, int32_t flags);
     binder::Status linkNativeLibraryDirectory(const std::unique_ptr<std::string>& uuid,
