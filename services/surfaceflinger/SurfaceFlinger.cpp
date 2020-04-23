e76fc0c4a0dc80d564ecd6b99ab5b6913e0cd65d
Lingfeng Yang <lfy@google.com>
2020-04-23 17:45:59 +0000

    Revert "[RenderEngine] Rebind output texture when unbinding framebuffer"
    This reverts commit d9c717511c46eefa78821a7e326bffde4f02076b.
    
    Reason for revert: Short-term, to unblock emulator users due to
    blank app drawer/recents: https://b.corp.google.com/issues/154665573
    
    Bug: b/140158384
    
    Change-Id: Ie3c6500b31eada4bf831ecef1a69c079db4cc61f

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2311,9 +2311,6 @@ void SurfaceFlinger::postComposition()
     }
     getBE().mLastSwapTime = currentTime;
 
-    // Cleanup any outstanding resources due to rendering a prior frame.
-    getRenderEngine().cleanupPostRender();
-
     {
         std::lock_guard lock(mTexturePoolMutex);
         if (mTexturePool.size() < mTexturePoolSize) {
