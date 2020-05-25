293aad3df5c6a13f9e787ad8289bf27c5c97d840
Pierre-Hugues Husson <phh@phh.me>
2020-05-25 10:12:24 +0000

    surfaceflinger: Add support for extension lib to get changed z order
    Change-Id: Id95aa73e06b4223a6b4f05c69fa2fc494f9a97b1

---
+++ services/surfaceflinger/CompositionEngine/include/compositionengine/FodExtension.h
@@ -0,0 +1,4 @@
+#ifndef __FOD_EXTENSION__H__
+#define __FOD_EXTENSION__H__
+extern uint32_t changedFodOrder(uint32_t z, bool touched);
+#endif /* __FOD_EXTENSION__H__ */
---
+++ services/surfaceflinger/CompositionEngine/src/FodExtension.cpp
@@ -0,0 +1,6 @@
+#include "FodExtension.h"
+
+__attribute__ ((weak))
+void changedFodOrder(uint32_t z, bool touched) {
+    return z;
+}
