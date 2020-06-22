e3668e11d88411700eba57f6ab8144d0d83e41d0
Jiyong Park <jiyong@google.com>
2020-06-22 23:07:58 +0900

    dalvik sysprop settings are optional
    dalvik sysprop settings in these mk files are changed to optional ones
    using the new 'a ?= b' syntax, to better express the intent that these
    provides safe default values which will be used when they are set in
    a product-specific mk file.
    
    This is also in preparation for
    I9c073a21c8257987cf2378012cadaeeeb698a4fb, which when lands will
    prohibit duplicated sysprop assignments.
    
    Bug: 117892318
    Bug: 158735147
    Test: m
    
    Exempt-From-Owner-Approval: cherry-pick from internal
    
    Merged-In: Ia3528c0c2f9715ff10b85b549d739c9263ab6a7e
    (cherry picked from commit 04f86d1cff620d3e7d1792795639ab0ca7edb715)
    Change-Id: Ia3528c0c2f9715ff10b85b549d739c9263ab6a7e

--- build/phone-hdpi-dalvik-heap.mk
+++ build/phone-hdpi-dalvik-heap.mk
@@ -16,9 +16,9 @@
 
 # Provides overrides to configure the Dalvik heap for a standard high density phone.
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=5m \
-    dalvik.vm.heapsize=32m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=2m
+PRODUCT_VENDOR_PROPERTIES += \
+    dalvik.vm.heapstartsize?=5m \
+    dalvik.vm.heapsize?=32m \
+    dalvik.vm.heaptargetutilization?=0.75 \
+    dalvik.vm.heapminfree?=512k \
+    dalvik.vm.heapmaxfree?=2m
--- build/tablet-dalvik-heap.mk
+++ build/tablet-dalvik-heap.mk
@@ -16,10 +16,10 @@
 
 # Provides overrides to configure the Dalvik heap for a standard tablet device.
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=5m \
-    dalvik.vm.heapgrowthlimit=48m \
-    dalvik.vm.heapsize=256m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=2m
+PRODUCT_VENDOR_PROPERTIES += \
+    dalvik.vm.heapstartsize?=5m \
+    dalvik.vm.heapgrowthlimit?=48m \
+    dalvik.vm.heapsize?=256m \
+    dalvik.vm.heaptargetutilization?=0.75 \
+    dalvik.vm.heapminfree?=512k \
+    dalvik.vm.heapmaxfree?=2m
