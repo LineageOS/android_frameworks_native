7e03ad3c24f4e454f361b5566ea103fbcc460eb7
Tiger Huang <tigerhuang@google.com>
2020-09-23 02:06:11 +0800

    Remove system UI flag constants
    They won't be used by anyone.
    
    Bug: 149813814
    Test: build and presubmit
    Change-Id: I4deb099b8198e164fdc38c91774a84d2913a8f87

--- include/input/Input.h
+++ include/input/Input.h
@@ -141,14 +141,6 @@ enum {
 #define MAX_CONTROLLER_LEDS 4
 
 /*
- * SystemUiVisibility constants from View.
- */
-enum {
-    ASYSTEM_UI_VISIBILITY_STATUS_BAR_VISIBLE = 0,
-    ASYSTEM_UI_VISIBILITY_STATUS_BAR_HIDDEN = 0x00000001,
-};
-
-/*
  * Maximum number of pointers supported per motion event.
  * Smallest number of pointers is 1.
  * (We want at least 10 but some touch controllers obstensibly configured for 10 pointers
