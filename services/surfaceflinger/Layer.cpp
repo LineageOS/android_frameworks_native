c83889f3b7d0b5dbdbf448ca9a7adfba55ff2f2a
KaiChieh Chuang <kaichieh.chuang@mediatek.com>
2020-10-13 10:38:48 +0800

    SF: handle long waiting Layer sync point
    When Layer transaction deferred
    it wait for barrier layer's specific frame.
    
    When the target frame is delayed for a long time, e.g. 5 min
    the deferred transaction layer may queue up in mPendingStates,
    may up to 20000 in real case.
    
    When the target frame come, it will loop through mPendingStates
    and call popPendingState with mPendingStates.removeAt(0);
    which is an inefficient operator for Vector, which cause SWT.
    
    Change to use std::deque for mPendingStates & mPendingStatesSnapshot
    Add sync point timeout debug log.
    
    Bug: 170690571
    Test: boot up
    Test: LayerUpdateTest
    Test: setting pages in/out
    
    Change-Id: I17f3751836574c3691c7e5a1e6d2ea6c3fcd3903
    Merged-In: I17f3751836574c3691c7e5a1e6d2ea6c3fcd3903

--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -793,7 +793,9 @@ void Layer::pushPendingState() {
             // to be applied as per normal (no synchronization).
             mCurrentState.barrierLayer_legacy = nullptr;
         } else {
-            auto syncPoint = std::make_shared<SyncPoint>(mCurrentState.frameNumber_legacy, this);
+            auto syncPoint = std::make_shared<SyncPoint>(mCurrentState.frameNumber_legacy,
+                                                         this,
+                                                         barrierLayer);
             if (barrierLayer->addSyncPoint(syncPoint)) {
                 std::stringstream ss;
                 ss << "Adding sync point " << mCurrentState.frameNumber_legacy;
@@ -818,7 +820,7 @@ void Layer::popPendingState(State* stateToCommit) {
     ATRACE_CALL();
     *stateToCommit = mPendingStates[0];
 
-    mPendingStates.removeAt(0);
+    mPendingStates.pop_front();
     ATRACE_INT(mTransactionName.c_str(), mPendingStates.size());
 }
 
@@ -857,6 +859,7 @@ bool Layer::applyPendingStates(State* stateToCommit) {
                 mRemoteSyncPoints.pop_front();
             } else {
                 ATRACE_NAME("!frameIsAvailable");
+                mRemoteSyncPoints.front()->checkTimeoutAndLog();
                 break;
             }
         } else {
