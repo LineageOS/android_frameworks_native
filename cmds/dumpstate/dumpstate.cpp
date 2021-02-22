51156967b9636e32e5d798529e44bd2be6a4f3d3
Lais Andrade <lsandrade@google.com>
2021-02-22 19:21:35 +0000

    Use vibrator_service cmd for dumpstate
    The local service "vibrator" was replaced by the new "vibrator_manager".
    
    Fix: 180924179
    Test: manual
    Change-Id: I9d3921af2b3b0c31767090c86f0a3350a6d45b1a

--- cmds/dumpstate/dumpstate.cpp
+++ cmds/dumpstate/dumpstate.cpp
@@ -2417,7 +2417,9 @@ static void SendBroadcast(const std::string& action, const std::vector<std::stri
 
 static void Vibrate(int duration_ms) {
     // clang-format off
-    RunCommand("", {"cmd", "vibrator", "vibrate", "-f", std::to_string(duration_ms), "dumpstate"},
+    std::vector<std::string> args = {"cmd", "vibrator_manager", "synced", "-f", "-d", "dumpstate",
+                                     "oneshot", std::to_string(duration_ms)};
+    RunCommand("", args,
                CommandOptions::WithTimeout(10)
                    .Log("Vibrate: '%s'\n")
                    .Always()
