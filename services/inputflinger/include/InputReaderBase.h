1ba71cce6f2889bec7b50defc14cf6cac63a3b86
Christine Franks <christyfranks@google.com>
2021-04-07 14:37:42 -0700

    Add dynamic display id associations for keyboards
    Bug: 184615313
    Test: atest -a inputflinger_tests and atest -a libinput_tests
    Change-Id: I96eb6af186e001ff37d90db2436c7d60a269d4bf

--- services/inputflinger/include/InputReaderBase.h
+++ services/inputflinger/include/InputReaderBase.h
@@ -198,6 +198,10 @@ struct InputReaderConfiguration {
     // Used to determine which DisplayViewport should be tied to which InputDevice.
     std::unordered_map<std::string, uint8_t> portAssociations;
 
+    // The associations between input device names and display unique ids.
+    // Used to determine which DisplayViewport should be tied to which InputDevice.
+    std::unordered_map<std::string, std::string> uniqueIdAssociations;
+
     // The suggested display ID to show the cursor.
     int32_t defaultPointerDisplayId;
 
