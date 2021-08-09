26dacabe028c624af4674537a4347fa56d8de95c
Robin Lee <rgl@google.com>
2021-08-09 14:31:01 +0200

    Add a GaussianBlurFilter to SkiaRE using Skia blur
    This is using Skia's native blur algorithm which is a fully-correct blur
    using separate X/Y convolution passes in a pre-baked shader.
    
    The performance is significantly worse than Kawase or dual-filtered
    Kawase but it sets the high bar for accuracy and the low bar for
    performance.
    
    This can be switched with KawaseBlurFilter by updating the source in
    SkiaGLRenderEngine to instantiate GaussianBlurFilter instead.
    
    Test: atest BlurTests
    Bug: 185365391
    Change-Id: I29fba84e408c75bc8ef7011b6e40f45cb78eabab

--- libs/renderengine/skia/filters/GaussianBlurFilter.h
+++ libs/renderengine/skia/filters/GaussianBlurFilter.h
@@ -29,24 +29,17 @@ namespace renderengine {
 namespace skia {
 
 /**
- * This is an implementation of a Kawase blur, as described in here:
- * https://community.arm.com/cfs-file/__key/communityserver-blogs-components-weblogfiles/
- * 00-00-00-20-66/siggraph2015_2D00_mmg_2D00_marius_2D00_notes.pdf
+ * This is an implementation of a Gaussian blur using Skia's built-in GaussianBlur filter.
  */
-class KawaseBlurFilter: public BlurFilter {
+class GaussianBlurFilter: public BlurFilter {
 public:
-    // Maximum number of render passes
-    static constexpr uint32_t kMaxPasses = 4;
-
-    explicit KawaseBlurFilter();
-    virtual ~KawaseBlurFilter(){}
+    explicit GaussianBlurFilter();
+    virtual ~GaussianBlurFilter(){}
 
     // Execute blur, saving it to a texture
     sk_sp<SkImage> generate(GrRecordingContext* context, const uint32_t radius,
                             const sk_sp<SkImage> blurInput, const SkRect& blurRect) const override;
 
-private:
-    sk_sp<SkRuntimeEffect> mBlurEffect;
 };
 
 } // namespace skia
