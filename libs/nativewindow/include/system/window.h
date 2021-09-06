1a467c28f7c3d3c4baf027269609da6729855847
Pablo Gamito <pablogamito@google.com>
2021-09-06 17:43:06 +0000

    Update ANativeWindow_getNextFrameId docs to specify API contract
    We rely on frame ids starting at 1, with 0 being invalid/uninitialize, and incrementing for each frame chronologically for as long as the underlying surface doesn't change.
    
    Test: N/A
    Change-Id: I90e9593b7ae852bcc90c5c17bc4969c7d827b2d9

--- libs/nativewindow/include/system/window.h
+++ libs/nativewindow/include/system/window.h
@@ -1089,7 +1089,10 @@ static inline int ANativeWindow_getLastQueuedBuffer2(ANativeWindow* window,
 /**
  * Retrieves an identifier for the next frame to be queued by this window.
  *
- * \return the next frame id.
+ * Frame ids start at 1 and are incremented on each new frame until the underlying surface changes,
+ * in which case the frame id is reset to 1.
+ *
+ * \return the next frame id (0 being uninitialized).
  */
 static inline uint64_t ANativeWindow_getNextFrameId(ANativeWindow* window) {
     uint64_t value;
