9c2d28cfb0bccaef2226cc9bdc4bcf84c842e60c
Prabir Pradhan <prabirmsp@google.com>
2021-09-29 17:42:14 +0000

    Reland^2 "Enable input window rotation flag"
    7719c7d2437171385cd692338748af583b824565
    
    This may cause some tests to fail because of b/201777251, but since we
    don't have a good idea of which/how many tests are affected, we can
    judge the impact after landing.
    
    Bug: 179274888
    Test: manual
    
    Change-Id: Ic49d0fe8024d4fcf4eab4ebe1a4a5189d8a3cec7

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -3096,7 +3096,7 @@ void SurfaceFlinger::updateInputFlinger() {
 
 bool enablePerWindowInputRotation() {
     static bool value =
-            android::base::GetBoolProperty("persist.debug.per_window_input_rotation", false);
+            android::base::GetBoolProperty("persist.debug.per_window_input_rotation", true);
     return value;
 }
 
