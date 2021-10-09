12965dd4c38a78c635afc8328e4c539a1f592a09
Luca Stefani <luca.stefani.ge1@gmail.com>
2021-10-09 12:03:19 +0200

    powermanager: Make Lineage HAL use its own mutex
    Change-Id: Ide34d8e4aeec18820f8ca5ad73021bfa49c33ef3

--- services/powermanager/PowerHalController.cpp
+++ services/powermanager/PowerHalController.cpp
@@ -56,6 +56,7 @@ std::unique_ptr<HalWrapper> HalConnector::connectLineage() {
 
 void HalConnector::reset() {
     PowerHalLoader::unloadAll();
+    PowerHalLoader::unloadLineage();
 }
 
 // -------------------------------------------------------------------------------------------------
@@ -82,7 +83,7 @@ std::shared_ptr<HalWrapper> PowerHalController::initHal() {
 // Check validity of current handle to the Lineage power HAL service, and create a new
 // one if necessary.
 std::shared_ptr<HalWrapper> PowerHalController::initLineageHal() {
-    std::lock_guard<std::mutex> lock(mConnectedHalMutex);
+    std::lock_guard<std::mutex> lock(mConnectedLineageHalMutex);
     if (mConnectedLineageHal == nullptr) {
         mConnectedLineageHal = mHalConnector->connect();
         if (mConnectedLineageHal == nullptr) {
