f83160f3f022ee04a68f1293631e0898ba5bb52f
Sally Qi <sallyqi@google.com>
2021-12-07 11:41:05 -0800

    Add DEPTH variables into ADataSpace
    Bug: 205034862
    Test: builds
    Change-Id: I1ac30853a873744027c2ea0aac731ecb76ca3b34

--- libs/nativewindow/ANativeWindow.cpp
+++ libs/nativewindow/ANativeWindow.cpp
@@ -176,6 +176,8 @@ int32_t ANativeWindow_setBuffersDataSpace(ANativeWindow* window, int32_t dataSpa
         static_cast<int>(HAL_DATASPACE_BT2020_HLG));
     static_assert(static_cast<int>(ADATASPACE_BT2020_ITU_HLG) ==
         static_cast<int>(HAL_DATASPACE_BT2020_ITU_HLG));
+    static_assert(static_cast<int>(DEPTH) == static_cast<int>(HAL_DATASPACE_DEPTH));
+    static_assert(static_cast<int>(DYNAMIC_DEPTH) == static_cast<int>(HAL_DATASPACE_DYNAMIC_DEPTH));
 
     if (!window || !query(window, NATIVE_WINDOW_IS_VALID) ||
             !isDataSpaceValid(window, dataSpace)) {
