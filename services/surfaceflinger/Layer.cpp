6582eb06eb5716dcf9eace53e6961788e0ac9e37
Linus Tufvesson <lus@google.com>
2022-03-04 09:32:07 +0000

    Allow windowhandles with NO_INPUT_CHANNEL - DO NOT MERGE
    This allows container surfaces to be considered visible in
    InputDispatcher and prevent touches from passing through them. In
    particular this is used by ActivityRecordInputSink to block touches that
    would otherwise pass through the area available to the activity.
    
    Bug: 194480991
    Bug: 222292477
    Test: Manually tested that blocking still works
    Test: atest InputSurfacesTest
    Change-Id: Iacfc952139311363b914b1d6bffc2b4190133d02
    (cherry picked from commit a18588206c240b34128bb27e3ac875af17fce7f2)
    (cherry picked from commit 4c0e43192b7b9dda53e49fd3a41ba5efc1980366)
    Merged-In: Iacfc952139311363b914b1d6bffc2b4190133d02

--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -2165,7 +2165,7 @@ void Layer::writeToProtoCommonState(LayerProto* layerInfo, LayerVector::StateSet
         layerInfo->set_owner_uid(mOwnerUid);
     }
 
-    if (traceFlags & SurfaceTracing::TRACE_INPUT) {
+    if (traceFlags & SurfaceTracing::TRACE_INPUT && needsInputInfo()) {
         WindowInfo info;
         if (useDrawing) {
             info = fillInputInfo({nullptr});
@@ -2469,7 +2469,8 @@ sp<Layer> Layer::getClonedRoot() {
 }
 
 bool Layer::hasInputInfo() const {
-    return mDrawingState.inputInfo.token != nullptr;
+    return mDrawingState.inputInfo.token != nullptr ||
+            mDrawingState.inputInfo.inputFeatures.test(WindowInfo::Feature::NO_INPUT_CHANNEL);
 }
 
 bool Layer::canReceiveInput() const {
