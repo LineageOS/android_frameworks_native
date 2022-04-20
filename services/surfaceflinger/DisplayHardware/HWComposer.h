ef0d08a24268ab125daab2ef3c88719c3f85d184
Rachel Lee <rnlee@google.com>
2022-04-20 17:40:37 +0000

    Revert "Avoid vsync enabled when in DOZE mode (AOD)."
    This reverts commit 8f96ec4b3074b6c1a4cb47bef4855f38896eb12c.
    
    Reason for revert: b/229336134#comment13
    
    Change-Id: Id2bcf9be0bbb19e0595a22441e3fb3e3fea0976d

--- services/surfaceflinger/DisplayHardware/HWComposer.h
+++ services/surfaceflinger/DisplayHardware/HWComposer.h
@@ -454,8 +454,6 @@ private:
         std::mutex vsyncEnabledLock;
         hal::Vsync vsyncEnabled GUARDED_BY(vsyncEnabledLock) = hal::Vsync::DISABLE;
 
-        hal::PowerMode powerMode = hal::PowerMode::ON;
-
         nsecs_t lastHwVsync = 0;
     };
 
