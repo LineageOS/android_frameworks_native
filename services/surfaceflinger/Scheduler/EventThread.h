28897b0cb3247dd575456103124ebad00d41d1b5
Ady Abraham <adyabr@google.com>
2022-06-06 15:16:06 -0700

    RESTRICT AUTOMERGE SurfaceFlinger: fix a potential race condition in stealReceiveChannel
    Add a mutex to prevent a potential race condition.
    
    Bug: 232541124
    Test: See bug for details
    Change-Id: Ia338f124c786bf12d6adba10a67b9048fe9c34a5
    (cherry picked from commit 0840cdd08f39994e3f8c58eb65f24a8db1dc1173)
    Merged-In: Ia338f124c786bf12d6adba10a67b9048fe9c34a5

--- services/surfaceflinger/Scheduler/EventThread.h
+++ services/surfaceflinger/Scheduler/EventThread.h
@@ -88,7 +88,8 @@ public:
 private:
     virtual void onFirstRef();
     EventThread* const mEventThread;
-    gui::BitTube mChannel;
+    std::mutex mLock;
+    gui::BitTube mChannel GUARDED_BY(mLock);
 };
 
 class EventThread {
