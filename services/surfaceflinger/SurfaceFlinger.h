4bcd690234ecabbc6e24865c3bdcd3acef590652
Rheygine Medel <quic_rmedel@quicinc.com>
2022-06-30 11:21:55 -0700

    sf: Allow WFD to use hwc in vendor frozen targets
    Change-Id: Iecd2be3eb8775855d82763f069a38bb70fe53d4e
    CRs-Fixed: 3229968

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -1503,6 +1503,7 @@ private:
 
     bool mAllowHwcForVDS = false;
     bool mAllowHwcForWFD = false;
+    int mFirstApiLevel = 0;
 
     // returns the framerate of the layer with the given sequence ID
     float getLayerFramerate(nsecs_t now, int32_t id) const {
