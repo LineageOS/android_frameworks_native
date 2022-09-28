2fb3dd6f5b3e9aa2d3544fd1e4818a75b65b0481
Kevin Jeon <kevinjeon@google.com>
2022-09-28 18:48:55 +0000

    Reduce timeout for DUMP NETSTATS PROTO
    This change reduces the timeout on 'dumpsys netstats --proto' from 120s
    to 5s. This is because go/bugreport-durations shows that the DUMP
    NETSTATS PROTO is always under 1s unless it stalls, in which case it
    will always hit the timeout.
    
    Ignore-AOSP-First: The netstats section only exists on internal builds.
    Test: Collect a bug report with 'adb bugreportz' to verify that the
          'DUMP NETSTATS PROTO' section still exists.
    Bug: 249522178
    Change-Id: I377bdb0ea61b53e76b6329fc52d634d1c2c0b0ad

--- cmds/dumpstate/dumpstate.cpp
+++ cmds/dumpstate/dumpstate.cpp
@@ -1049,7 +1049,7 @@ static void DumpNetstatsProto() {
         return;
     }
     RunCommandToFd(fd, "", {"dumpsys", "netstats", "--proto"},
-            CommandOptions::WithTimeout(120).Build());
+            CommandOptions::WithTimeout(5).Build());
     bool empty = 0 == lseek(fd, 0, SEEK_END);
     if (!empty) {
         ds.EnqueueAddZipEntryAndCleanupIfNeeded(kProtoPath + "netstats" + kProtoExt,
