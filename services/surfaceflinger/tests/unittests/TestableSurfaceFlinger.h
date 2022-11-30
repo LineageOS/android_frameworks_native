c132a15668ee26b5c6e193f3898808a80174c57b
Dominik Laskowski <domlaskowski@google.com>
2022-11-30 15:44:52 -0500

    SF: Fix condition to activate display on power-on
    Since [1], getDefaultDisplayDeviceLocked's fallback when the active
    display is outdated is to return the primary display, not `nullptr`.
    During the initial call to setPowerModeInternal, `activeDisplay` is
    no longer `nullptr`, but the primary display.
    
    After [2], onActiveDisplayChangedLocked is expected to be called for
    the primary display during boot. This was not the case though, since
    the DisplayDevice::setPowerMode() update precedes the DisplayDevice::
    isPoweredOn() query.
    
    Fix this by querying before updating. Also, clean up `nullptr` checks
    around onActiveDisplayChangedLocked.
    
    [1] If83c908446e5e5267dfcb15189a26b779d75b216
    [2] I15e0f5a280e62baf6d4e6ea2748d95342e79ac44
    
    Fixes: 260663220
    Test: The leader display ID is logged during single-display boot.
    Test: SetPowerModeInternalTest.designatesLeaderDisplay
    Change-Id: I0d20ce7f7ab3f9e90ef1c72263b8166865486d62

--- services/surfaceflinger/tests/unittests/TestableSurfaceFlinger.h
+++ services/surfaceflinger/tests/unittests/TestableSurfaceFlinger.h
@@ -470,7 +470,7 @@ public:
     void onActiveDisplayChanged(const sp<DisplayDevice>& activeDisplay) {
         Mutex::Autolock lock(mFlinger->mStateLock);
         ftl::FakeGuard guard(kMainThreadContext);
-        mFlinger->onActiveDisplayChangedLocked(activeDisplay);
+        mFlinger->onActiveDisplayChangedLocked(nullptr, activeDisplay);
     }
 
     auto createLayer(LayerCreationArgs& args, const sp<IBinder>& parentHandle,
@@ -621,7 +621,7 @@ public:
             return *this;
         }
 
-        auto& setPowerMode(hal::PowerMode mode) {
+        auto& setPowerMode(std::optional<hal::PowerMode> mode) {
             mPowerMode = mode;
             return *this;
         }
@@ -645,7 +645,11 @@ public:
                                                          mHwcDisplayType);
 
             display->mutableIsConnected() = true;
-            display->setPowerMode(mPowerMode);
+
+            if (mPowerMode) {
+                display->setPowerMode(*mPowerMode);
+            }
+
             flinger->mutableHwcDisplayData()[mDisplayId].hwcDisplay = std::move(display);
 
             EXPECT_CALL(*composer, getDisplayConfigs(mHwcDisplayId, _))
@@ -711,7 +715,7 @@ public:
         int32_t mDpiY = DEFAULT_DPI;
         int32_t mConfigGroup = DEFAULT_CONFIG_GROUP;
         hal::HWConfigId mActiveConfig = DEFAULT_ACTIVE_CONFIG;
-        hal::PowerMode mPowerMode = DEFAULT_POWER_MODE;
+        std::optional<hal::PowerMode> mPowerMode = DEFAULT_POWER_MODE;
         const std::unordered_set<aidl::android::hardware::graphics::composer3::Capability>*
                 mCapabilities = nullptr;
     };
@@ -788,7 +792,7 @@ public:
             return *this;
         }
 
-        auto& setPowerMode(hal::PowerMode mode) {
+        auto& setPowerMode(std::optional<hal::PowerMode> mode) {
             mCreationArgs.initialPowerMode = mode;
             return *this;
         }
@@ -852,6 +856,10 @@ public:
                 const auto physicalIdOpt = PhysicalDisplayId::tryCast(*displayId);
                 LOG_ALWAYS_FATAL_IF(!physicalIdOpt);
                 const auto physicalId = *physicalIdOpt;
+
+                if (mCreationArgs.isPrimary) {
+                    mFlinger.mutableActiveDisplayId() = physicalId;
+                }
 
                 LOG_ALWAYS_FATAL_IF(!mHwcDisplayId);
 
