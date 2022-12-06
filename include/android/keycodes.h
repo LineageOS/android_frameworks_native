58120357b446182f8a2a047be49a9f35789c0d50
Vaibhav Devmurari <vdevmurari@google.com>
2022-12-06 22:25:09 +0000

    Add new KEYCODE_RECENT_APPS and corresponding mappings
    Test: manual
    Bug: 261621522
    Change-Id: I282eb4b2b29a4790e287371ac9408120ff32997d

--- include/android/keycodes.h
+++ include/android/keycodes.h
@@ -829,6 +829,8 @@ enum {
     AKEYCODE_STYLUS_BUTTON_TERTIARY = 310,
     /** A button on the tail end of a stylus. */
     AKEYCODE_STYLUS_BUTTON_TAIL = 311,
+    /** Key to open recent apps (a.k.a. Overview) */
+    AKEYCODE_RECENT_APPS = 312,
 
     // NOTE: If you add a new keycode here you must also add it to several other files.
     //       Refer to frameworks/base/core/java/android/view/KeyEvent.java for the full list.
