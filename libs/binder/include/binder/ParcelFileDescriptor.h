littleGnAl/mirror_platform_frameworks_native

ParcelableHolder/ParcelFileDescriptor toString
Bug: 259162245
Test: N/A
Change-Id: I1479b7b58ba4ccd7a9bff43d7ce962e0f1b28678

libs/binder/include/binder/ParcelFileDescriptor.h
@@ -42,6 +42,7 @@ public:
     android::status_t writeToParcel(android::Parcel* parcel) const override;
     android::status_t readFromParcel(const android::Parcel* parcel) override;
 
+    inline std::string toString() const { return "pfd: " + std::to_string(get()); }
     inline bool operator!=(const ParcelFileDescriptor& rhs) const {
         return mFd.get() != rhs.mFd.get();
     }

LineageOS/android_frameworks_native

libs/binder/include/binder/ParcelFileDescriptor.h
@@ -42,6 +42,7 @@ public:
     android::status_t writeToParcel(android::Parcel* parcel) const override;
     android::status_t readFromParcel(const android::Parcel* parcel) override;
 
+    inline std::string toString() const { return "ParcelFileDescriptor:" + std::to_string(get()); }
     inline bool operator!=(const ParcelFileDescriptor& rhs) const {
         return mFd.get() != rhs.mFd.get();
     }