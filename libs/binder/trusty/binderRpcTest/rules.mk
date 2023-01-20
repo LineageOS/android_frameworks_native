littleGnAl/mirror_platform_frameworks_native

libbinder: Build and run binderRpcTest on Trusty
Refactor binderRpcTest to run some of its tests on Trusty.

Bug: 230135749
Test: run --boot-test com.android.trusty.binderRpcTest
Change-Id: I816f9b0a4494d173e6dd9a5415e4758c1a32933d

libs/binder/trusty/binderRpcTest/rules.mk
@@ -13,6 +13,22 @@
 # limitations under the License.
 #
 
-TRUSTY_USER_TESTS += \
-	frameworks/native/libs/binder/trusty/binderRpcTestService \
-	frameworks/native/libs/binder/trusty/binderRpcTest \
+LOCAL_DIR := $(GET_LOCAL_DIR)
+LIBBINDER_TESTS_DIR := frameworks/native/libs/binder/tests
+
+MODULE := $(LOCAL_DIR)
+
+MANIFEST := $(LOCAL_DIR)/manifest.json
+
+MODULE_SRCS += \
+	$(LIBBINDER_TESTS_DIR)/binderRpcTest.cpp \
+	$(LIBBINDER_TESTS_DIR)/binderRpcTestCommon.cpp \
+
+MODULE_LIBRARY_DEPS += \
+	$(LOCAL_DIR)/aidl \
+	trusty/user/base/lib/googletest \
+	trusty/user/base/lib/libstdc++-trusty \
+	frameworks/native/libs/binder/trusty \
+	frameworks/native/libs/binder/trusty/ndk \
+
+include make/trusted_app.mk

LineageOS/android_frameworks_native

Test: mmm tests

libs/binder/trusty/binderRpcTest/rules.mk
@@ -13,7 +13,23 @@
 # limitations under the License.
 #
 
-TRUSTY_USER_TESTS += \
-	frameworks/native/libs/binder/trusty/binderRpcTest \
-	frameworks/native/libs/binder/trusty/binderRpcTest/service \
+LOCAL_DIR := $(GET_LOCAL_DIR)
+LIBBINDER_TESTS_DIR := frameworks/native/libs/binder/tests
 
+MODULE := $(LOCAL_DIR)
+
+MANIFEST := $(LOCAL_DIR)/manifest.json
+
+MODULE_SRCS += \
+	$(LIBBINDER_TESTS_DIR)/binderRpcUniversalTests.cpp \
+	$(LIBBINDER_TESTS_DIR)/binderRpcTestCommon.cpp \
+	$(LIBBINDER_TESTS_DIR)/binderRpcTestTrusty.cpp \
+
+MODULE_LIBRARY_DEPS += \
+	$(LOCAL_DIR)/aidl \
+	frameworks/native/libs/binder/trusty \
+	frameworks/native/libs/binder/trusty/ndk \
+	trusty/user/base/lib/googletest \
+	trusty/user/base/lib/libstdc++-trusty \
+
+include make/trusted_app.mk