a692783e596bcdba847fcf65cc53fccd978d975d
Harry Cutts <hcutts@google.com>
2023-01-25 20:06:17 +0000

    Revert^2 "Enable new touchpad stack by default"
    This reverts commit e60fa698a2d69fcdcd4fb71eb1a0be0bef000507.
    
    Reason for revert: the issue behind the original revert (caused by the previous CL in the chain) has now been resolved.
    
    Bug: 251196347, 264996455
    Test: presubmit
    Change-Id: I4471d1e6bdd910b06b50a24d7999e2228983b4c5

--- services/inputflinger/reader/InputDevice.cpp
+++ services/inputflinger/reader/InputDevice.cpp
@@ -209,7 +209,7 @@ void InputDevice::addEventHubDevice(int32_t eventHubId, bool populateMappers) {
 
     // Touchscreens and touchpad devices.
     static const bool ENABLE_TOUCHPAD_GESTURES_LIBRARY =
-            sysprop::InputProperties::enable_touchpad_gestures_library().value_or(false);
+            sysprop::InputProperties::enable_touchpad_gestures_library().value_or(true);
     // TODO(b/246587538): Fix the new touchpad stack for Sony DualShock 4 (5c4, 9cc) and DualSense
     // (ce6) touchpads, or at least load this setting from the IDC file.
     const InputDeviceIdentifier& identifier = getDeviceInfo().getIdentifier();
