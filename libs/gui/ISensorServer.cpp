e26abe0dde5afcf5d15f2919774bbc305ccadeb8
Devin Moore <devinmoore@google.com>
2023-02-17 19:35:25 +0000

    Remove some new memory leaks from SensorManager
    After catching an error in Sensor::unflatten, there are memory leaks
    caught by the fuzzer in the same test case.
    
    Test: libsensorserviceaidl_fuzzer with testcase from bug
    Bug: 269014004
    Merged-In: I509cceb41f56ca117d9475f6f6674244560fe582
    Change-Id: I509cceb41f56ca117d9475f6f6674244560fe582
    (cherry picked from commit c95fa0f0e7c7b73746ff850b85a79fc5f92b784e)
    Merged-In: I509cceb41f56ca117d9475f6f6674244560fe582
    (cherry picked from commit on googleplex-android-review.googlesource.com host: ceb0d52273256c6a5c5622bf81b0ac4ba106faa1)
    Merged-In: I509cceb41f56ca117d9475f6f6674244560fe582

--- libs/gui/ISensorServer.cpp
+++ libs/gui/ISensorServer.cpp
@@ -61,7 +61,11 @@ public:
         v.setCapacity(n);
         while (n) {
             n--;
-            reply.read(s);
+            if(reply.read(s) != OK) {
+                ALOGE("Failed to read reply from getSensorList");
+                v.clear();
+                break;
+            }
             v.add(s);
         }
         return v;
@@ -79,7 +83,11 @@ public:
         v.setCapacity(n);
         while (n) {
             n--;
-            reply.read(s);
+            if(reply.read(s) != OK) {
+                ALOGE("Failed to read reply from getDynamicSensorList");
+                v.clear();
+                break;
+            }
             v.add(s);
         }
         return v;
