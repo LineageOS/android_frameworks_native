71ddb4376475db6bb77d32aa6aa3ce56a6ce20e0
Justin Chung <justinkchung@google.com>
2023-03-27 04:29:07 +0000

    Keep touch mode for the key event triggered by gesture
    Bug: 267391997
    Test: atest --host libinput_tests inputflinger_tests
    Change-Id: I686cb8f9dff01f5964f40518ea60509d806b8d8f

--- services/inputflinger/tests/InputReader_test.cpp
+++ services/inputflinger/tests/InputReader_test.cpp
@@ -3651,6 +3651,19 @@ TEST_F(KeyboardInputMapperTest, LayoutInfoCorrectlyMapped) {
     ASSERT_EQ("extended", mDevice->getDeviceInfo().getKeyboardLayoutInfo()->layoutType);
 }
 
+TEST_F(KeyboardInputMapperTest, Process_GesureEventToSetFlagKeepTouchMode) {
+    mFakeEventHub->addKey(EVENTHUB_ID, KEY_LEFT, 0, AKEYCODE_DPAD_LEFT, POLICY_FLAG_GESTURE);
+    KeyboardInputMapper& mapper =
+            addMapperAndConfigure<KeyboardInputMapper>(AINPUT_SOURCE_KEYBOARD,
+                                                       AINPUT_KEYBOARD_TYPE_ALPHABETIC);
+    NotifyKeyArgs args;
+
+    // Key down
+    process(mapper, ARBITRARY_TIME, READ_TIME, EV_KEY, KEY_LEFT, 1);
+    ASSERT_NO_FATAL_FAILURE(mFakeListener->assertNotifyKeyWasCalled(&args));
+    ASSERT_EQ(AKEY_EVENT_FLAG_FROM_SYSTEM | AKEY_EVENT_FLAG_KEEP_TOUCH_MODE, args.flags);
+}
+
 // --- KeyboardInputMapperTest_ExternalDevice ---
 
 class KeyboardInputMapperTest_ExternalDevice : public InputMapperTest {
