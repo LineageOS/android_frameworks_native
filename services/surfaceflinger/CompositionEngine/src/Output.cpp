43b5d521210b89dbc72c3c5cca8c10454fad3348
Leon Scroggins III <scroggo@google.com>
2023-04-10 15:53:45 -0400

    Skip renderCachedSets if the Output is not enabled
    This method was being called for a display that had not turned on yet.
    The call to getSkipColorTransform resulted in a log warning - the call
    only checks a cached value, and the value is only cached when the
    display is first turned on. Skip the call when it is not enabled (e.g.
    when it is off), removing the log spam.
    
    Bug: 277358841
    Test: look at logs
    Change-Id: Ic5eb3a69d50719cf68e3b41d6eca659bd712f65f

--- services/surfaceflinger/CompositionEngine/src/Output.cpp
+++ services/surfaceflinger/CompositionEngine/src/Output.cpp
@@ -1574,9 +1574,10 @@ void Output::postFramebuffer() {
 }
 
 void Output::renderCachedSets(const CompositionRefreshArgs& refreshArgs) {
-    if (mPlanner) {
-        mPlanner->renderCachedSets(getState(), refreshArgs.scheduledFrameTime,
-                                   getState().usesDeviceComposition || getSkipColorTransform());
+    const auto& outputState = getState();
+    if (mPlanner && outputState.isEnabled) {
+        mPlanner->renderCachedSets(outputState, refreshArgs.scheduledFrameTime,
+                                   outputState.usesDeviceComposition || getSkipColorTransform());
     }
 }
 
