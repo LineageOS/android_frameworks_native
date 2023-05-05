3fea8b580fd4e857b2e89cc5404b4a73a3e8d42f
Kunal Malhotra <malhk@google.com>
2023-05-05 22:42:05 +0000

    Updating CPP ActivityManager interface to be one way
    Ignore-AOSP-First: This code will go into AOSP later with U.
    Test: manual testing on device
    Bug: 279787820
    Change-Id: I53b895982816144c00290bbb550b6bb290d86c30

--- libs/binder/IActivityManager.cpp
+++ libs/binder/IActivityManager.cpp
@@ -138,7 +138,8 @@ public:
         data.writeInt32(apiType);
         data.writeInt32(appUid);
         data.writeInt32(appPid);
-        status_t err = remote()->transact(LOG_FGS_API_BEGIN_TRANSACTION, data, &reply);
+        status_t err = remote()->transact(LOG_FGS_API_BEGIN_TRANSACTION, data, &reply,
+                                          IBinder::FLAG_ONEWAY);
         if (err != NO_ERROR || ((err = reply.readExceptionCode()) != NO_ERROR)) {
             ALOGD("FGS Logger Transaction failed");
             ALOGD("%d", err);
@@ -153,7 +154,8 @@ public:
         data.writeInt32(apiType);
         data.writeInt32(appUid);
         data.writeInt32(appPid);
-        status_t err = remote()->transact(LOG_FGS_API_END_TRANSACTION, data, &reply);
+        status_t err =
+                remote()->transact(LOG_FGS_API_END_TRANSACTION, data, &reply, IBinder::FLAG_ONEWAY);
         if (err != NO_ERROR || ((err = reply.readExceptionCode()) != NO_ERROR)) {
             ALOGD("FGS Logger Transaction failed");
             ALOGD("%d", err);
@@ -170,7 +172,8 @@ public:
         data.writeInt32(state);
         data.writeInt32(appUid);
         data.writeInt32(appPid);
-        status_t err = remote()->transact(LOG_FGS_API_BEGIN_TRANSACTION, data, &reply);
+        status_t err = remote()->transact(LOG_FGS_API_BEGIN_TRANSACTION, data, &reply,
+                                          IBinder::FLAG_ONEWAY);
         if (err != NO_ERROR || ((err = reply.readExceptionCode()) != NO_ERROR)) {
             ALOGD("FGS Logger Transaction failed");
             ALOGD("%d", err);
