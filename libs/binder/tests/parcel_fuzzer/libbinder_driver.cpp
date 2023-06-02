57b1591dca003b488d42e85dfdc5e096d35fdf8e
Steven Moreland <smoreland@google.com>
2023-06-02 21:35:00 +0000

    libbinder_driver: never enforce data avail
    It would be good to cover this too, but we do have it covered
    via binder_parcel_fuzzer now, and this causes a lot of wasted
    cycles for our AIDL fuzzers.
    
    Bug: N/A
    Test: diffing strace of runs before/after
    Change-Id: I743b62272c402c400c5e357adf5538dc5440f2a6

--- libs/binder/tests/parcel_fuzzer/libbinder_driver.cpp
+++ libs/binder/tests/parcel_fuzzer/libbinder_driver.cpp
@@ -52,7 +52,7 @@ void fuzzService(const std::vector<sp<IBinder>>& binders, FuzzedDataProvider&& p
                     uint32_t flags = provider.ConsumeIntegral<uint32_t>();
                     Parcel data;
                     // for increased fuzz coverage
-                    data.setEnforceNoDataAvail(provider.ConsumeBool());
+                    data.setEnforceNoDataAvail(false);
 
                     sp<IBinder> target = options.extraBinders.at(
                             provider.ConsumeIntegralInRange<size_t>(0,
@@ -73,7 +73,7 @@ void fuzzService(const std::vector<sp<IBinder>>& binders, FuzzedDataProvider&& p
 
                     Parcel reply;
                     // for increased fuzz coverage
-                    reply.setEnforceNoDataAvail(provider.ConsumeBool());
+                    reply.setEnforceNoDataAvail(false);
                     (void)target->transact(code, data, &reply, flags);
 
                     // feed back in binders and fds that are returned from the service, so that
