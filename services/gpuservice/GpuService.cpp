a8186037a730699b53e49de7241280c7532e5fc4
sergiuferentz <sergiuferentz@google.com>
2023-06-26 18:01:47 +0000

    Fix for heap-use-after-free in GPUService.cpp
    This adds a unit test and fix for the bug reported by libfuzzer.
    Changes made:
     * Expose GPUService as testable code.
     * Update main_gpuservice.cpp to use the new GpuService now located at
       gpuservice/GpuService.h
     * Make initializer threads members of GpuService
     * Join the threads in destructor to prevent heap-use-after-free.
     * Add unit test that waits 3 seconds after deallocation to ensure no
       wrong access is made.
    
    Merged-In: I4d1d2d4658b575bf2c8f425f91f68f03114ad029
    Bug: 282919145
    Test: Added unit test and ran on device with ASAN
    Change-Id: I4d1d2d4658b575bf2c8f425f91f68f03114ad029
    (cherry picked from commit 3c00cbc0f119c3f59325aa6d5061529feb58462b)

--- services/gpuservice/GpuService.cpp
+++ services/gpuservice/GpuService.cpp
@@ -16,7 +16,7 @@
 
 #define ATRACE_TAG ATRACE_TAG_GRAPHICS
 
-#include "GpuService.h"
+#include "gpuservice/GpuService.h"
 
 #include <android-base/stringprintf.h>
 #include <android-base/properties.h>
@@ -35,6 +35,7 @@
 #include <vkjson.h>
 
 #include <thread>
+#include <memory>
 
 namespace android {
 
@@ -58,17 +59,20 @@ GpuService::GpuService()
         mGpuStats(std::make_unique<GpuStats>()),
         mGpuMemTracer(std::make_unique<GpuMemTracer>()) {
 
-    std::thread gpuMemAsyncInitThread([this]() {
+    mGpuMemAsyncInitThread = std::make_unique<std::thread>([this] (){
         mGpuMem->initialize();
         mGpuMemTracer->initialize(mGpuMem);
     });
-    gpuMemAsyncInitThread.detach();
 
-    std::thread gpuWorkAsyncInitThread([this]() {
+    mGpuWorkAsyncInitThread = std::make_unique<std::thread>([this]() {
         mGpuWork->initialize();
     });
-    gpuWorkAsyncInitThread.detach();
 };
+
+GpuService::~GpuService() {
+    mGpuWorkAsyncInitThread->join();
+    mGpuMemAsyncInitThread->join();
+}
 
 void GpuService::setGpuStats(const std::string& driverPackageName,
                              const std::string& driverVersionName, uint64_t driverVersionCode,
