1a467a5dcad5b369f1e1a5d099f2294d0d9d055e
sergiuferentz <sergiuferentz@google.com>
2023-06-26 18:01:47 +0000

    Fix for heap-use-after-free in GPUService.cpp
    This adds a unit test and fix for the bug reported by libfuzzer.
    Changes made:
     * Expose GPUService as testable code.
     * Update main_gpuservice.cpp to use the new GpuService now located at
       gpuservice/GpuService.h
     * Make initializer threads members of GpuService
     * Join the threads in destructor to prevent heap-use-after-free.
     * Add unit test that waits 3 seconds after deallocation to ensure no
       wrong access is made.
    
    Bug: 282919145
    Test: Added unit test and ran on device with ASAN
    (cherry picked from commit 3c00cbc0f119c3f59325aa6d5061529feb58462b)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:24a7874bb4093a1a6721a2002569512c43af5bdc)
    Merged-In: I4d1d2d4658b575bf2c8f425f91f68f03114ad029
    Change-Id: I4d1d2d4658b575bf2c8f425f91f68f03114ad029

--- services/gpuservice/include/gpuservice/GpuService.h
+++ services/gpuservice/include/gpuservice/GpuService.h
@@ -24,6 +24,7 @@
 #include <serviceutils/PriorityDumper.h>
 
 #include <mutex>
+#include <thread>
 #include <vector>
 
 namespace android {
@@ -37,6 +38,7 @@ public:
     static const char* const SERVICE_NAME ANDROID_API;
 
     GpuService() ANDROID_API;
+    ~GpuService();
 
 protected:
     status_t shellCommand(int in, int out, int err, std::vector<String16>& args) override;
@@ -81,6 +83,7 @@ private:
     std::unique_ptr<GpuMemTracer> mGpuMemTracer;
     std::mutex mLock;
     std::string mDeveloperDriverPath;
+    std::unique_ptr<std::thread> mGpuMemAsyncInitThread;
 };
 
 } // namespace android
