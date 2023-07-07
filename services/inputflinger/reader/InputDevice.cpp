infinity-mojito/frameworks_native

InputMapper refactor: Revert "fix touch issue on portrait reference"
This reverts commit 0987fd8824d38e8bb4642fec705842a1c27bace8.

Test: m checkinput && atest libinput_tests inputflinger_tests
Bug: 256009910
Change-Id: I452b8292a5a797f21707f56ee01765650db99b94

services/inputflinger/reader/InputDevice.cpp
@@ -504,9 +504,9 @@ std::vector<std::unique_ptr<InputMapper>> InputDevice::createMappers(
         classes.test(InputDeviceClass::TOUCH_MT) && !isSonyDualShock4Touchpad) {
         mappers.push_back(createInputMapper<TouchpadInputMapper>(context, readerConfig));
     } else if (classes.test(InputDeviceClass::TOUCH_MT)) {
-        mappers.push_back(std::make_unique<MultiTouchInputMapper>(context, readerConfig));
+        mappers.push_back(createInputMapper<MultiTouchInputMapper>(context, readerConfig));
     } else if (classes.test(InputDeviceClass::TOUCH)) {
-        mappers.push_back(std::make_unique<SingleTouchInputMapper>(context, readerConfig));
+        mappers.push_back(createInputMapper<SingleTouchInputMapper>(context, readerConfig));
     }
 
     // Joystick-like devices.

LineageOS/android_frameworks_native

Change-Id: I7d4d8de41e91d57092c1226a0a340b8decffa32b

services/inputflinger/reader/InputDevice.cpp
@@ -500,9 +500,9 @@ std::vector<std::unique_ptr<InputMapper>> InputDevice::createMappers(
         classes.test(InputDeviceClass::TOUCH_MT) && !isSonyDualShock4Touchpad) {
         mappers.push_back(createInputMapper<TouchpadInputMapper>(contextPtr, readerConfig));
     } else if (classes.test(InputDeviceClass::TOUCH_MT)) {
-        mappers.push_back(std::make_unique<MultiTouchInputMapper>(contextPtr, readerConfig));
+        mappers.push_back(createInputMapper<MultiTouchInputMapper>(contextPtr, readerConfig));
     } else if (classes.test(InputDeviceClass::TOUCH)) {
-        mappers.push_back(std::make_unique<SingleTouchInputMapper>(contextPtr, readerConfig));
+        mappers.push_back(createInputMapper<SingleTouchInputMapper>(contextPtr, readerConfig));
     }
 
     // Joystick-like devices.