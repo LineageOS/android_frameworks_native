8a0e35288015d18061d3acea6ea66f1d3b44f438
Tomasz Wasilczyk <twasilczyk@google.com>
2023-08-14 18:03:09 +0000

    Migrate from android::String isEmpty to empty
    This empty method is different from the old one - it aligns with
    std::string definition.
    
    Bug: 295394788
    Test: make checkbuild
    Change-Id: Id74502de0e51182aa9629db8dc70739be4483d12
    Merged-In: Id74502de0e51182aa9629db8dc70739be4483d12

--- libs/input/KeyCharacterMap.cpp
+++ libs/input/KeyCharacterMap.cpp
@@ -1345,7 +1345,7 @@ status_t KeyCharacterMap::Parser::parseCharacterLiteral(char16_t* outCharacter) 
     }
 
     // Ensure that we consumed the entire token.
-    if (mTokenizer->nextToken(WHITESPACE).isEmpty()) {
+    if (mTokenizer->nextToken(WHITESPACE).empty()) {
         return NO_ERROR;
     }
 
