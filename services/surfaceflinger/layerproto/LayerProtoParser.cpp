b194d7ba3c207505c5eeafa407e3c3e256461da2
Diwas Sharma <sdiwas@google.com>
2023-09-01 00:43:15 +0000

    Revert "Revert layer proto format back to proto2"
    This reverts commit 5dad909092932dc5c02d1be2f6ece1a3d06908ad.
    
    Reason for revert: 298512106
    
    Change-Id: I6c371f9958b2112f51ba801131ee912dff5a4da7

--- services/surfaceflinger/layerproto/LayerProtoParser.cpp
+++ services/surfaceflinger/layerproto/LayerProtoParser.cpp
@@ -193,13 +193,13 @@ void LayerProtoParser::updateChildrenAndRelative(const perfetto::protos::LayerPr
         }
     }
 
-    if (layerProto.has_parent()) {
+    if (layerProto.parent() != -1) {
         if (layerMap.count(layerProto.parent()) > 0) {
             currLayer->parent = layerMap[layerProto.parent()];
         }
     }
 
-    if (layerProto.has_z_order_relative_of()) {
+    if (layerProto.z_order_relative_of() != -1) {
         if (layerMap.count(layerProto.z_order_relative_of()) > 0) {
             currLayer->zOrderRelativeOf = layerMap[layerProto.z_order_relative_of()];
         }
