627a733000f50a9f85f1b63aa3dc148645ed70d3
Michael Bestas <mkbestas@lineageos.org>
2024-01-11 00:27:37 +0200

    fixup! libui: Allow extension of valid gralloc 1.0 buffer usage bits
    Change-Id: I681f702f02a1e9b04c1e91d3f975f3ade12714f8

--- libs/ui/Gralloc2.cpp
+++ libs/ui/Gralloc2.cpp
@@ -49,11 +49,11 @@ uint64_t getValid10UsageBits() {
             bits = bits | bit;
         }
 
-#ifdef ADDNL_GRALLOC_10_USAGE_BITS
-        uint64_t addnl_bits = static_cast<uint64_t>(ADDNL_GRALLOC_10_USAGE_BITS);
-        ALOGI("Adding additional valid usage bits: 0x%" PRIx64, addnl_bits);
-        bits = bits | addnl_bits;
-#endif
+        if (ADDNL_GRALLOC_10_USAGE_BITS) {
+            uint64_t addnl_bits = static_cast<uint64_t>(ADDNL_GRALLOC_10_USAGE_BITS);
+            ALOGI("Adding additional valid usage bits: 0x%" PRIx64, addnl_bits);
+            bits = bits | addnl_bits;
+        }
 
         return bits;
     }();
