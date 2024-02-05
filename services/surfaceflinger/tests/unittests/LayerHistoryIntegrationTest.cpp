08e7fe6bc06166a9120f99f8a302e114a352b647
Arthur Hung <arthurhung@google.com>
2024-02-05 08:09:27 +0000

    Update tests to treat enable_small_area_detection as true
    Explictitly set enable_small_area_detection flag to true so the tests
    could always pass.
    
    Bug: 323232586
    Test: libsurfaceflinger_unittest
    Change-Id: Ie21becc71f968fa8658824ef143dbb7fe4f110a1

--- services/surfaceflinger/tests/unittests/LayerHistoryIntegrationTest.cpp
+++ services/surfaceflinger/tests/unittests/LayerHistoryIntegrationTest.cpp
@@ -1026,6 +1026,7 @@ protected:
 };
 
 TEST_F(SmallAreaDetectionTest, SmallDirtyLayer) {
+    SET_FLAG_FOR_TEST(flags::enable_small_area_detection, true);
     auto layer = createLegacyAndFrontedEndLayer(1);
 
     nsecs_t time = systemTime();
@@ -1043,6 +1044,7 @@ TEST_F(SmallAreaDetectionTest, SmallDirtyLayer) {
 }
 
 TEST_F(SmallAreaDetectionTest, NotSmallDirtyLayer) {
+    SET_FLAG_FOR_TEST(flags::enable_small_area_detection, true);
     auto layer = createLegacyAndFrontedEndLayer(1);
 
     nsecs_t time = systemTime();
@@ -1060,6 +1062,7 @@ TEST_F(SmallAreaDetectionTest, NotSmallDirtyLayer) {
 }
 
 TEST_F(SmallAreaDetectionTest, smallDirtyLayerWithMatrix) {
+    SET_FLAG_FOR_TEST(flags::enable_small_area_detection, true);
     auto layer = createLegacyAndFrontedEndLayer(1);
 
     nsecs_t time = systemTime();
