238a5b5296cd2d9113fd566c7fda3a045260178b
Edwin Moquete <edwinmmoquete@gmail.com>
2024-03-26 16:55:59 +0100

    input: Bring back PointerProperties::copyFrom()
    libwfdnative compiled for 14 QPR1 depends on this symbol.
    
    Change-Id: I7bc5ee3fb7760a9f470023cd0daa215b2e194dc1

--- include/input/Input.h
+++ include/input/Input.h
@@ -513,6 +513,8 @@ struct PointerProperties {
     }
 
     PointerProperties& operator=(const PointerProperties&) = default;
+
+    void copyFrom(const PointerProperties& other);
 };
 
 // TODO(b/211379801) : Use a strong type from ftl/mixins.h instead
