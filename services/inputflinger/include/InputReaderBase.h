littleGnAl/mirror_platform_frameworks_native

Allow excluding devices via VID/PID
Change-Id: Ib1ea94881852d378e20b331b283e2b904c08b891
Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>

services/inputflinger/include/InputReaderBase.h
@@ -196,6 +196,10 @@ struct InputReaderConfiguration {
     // Devices with these names will be ignored.
     std::vector<std::string> excludedDeviceNames;
 
+    // The excluded device vid/pid for the platform.
+    // Devices with these vid/pid combo will be ignored.
+    std::vector<std::pair<uint16_t, uint16_t>> excludedDeviceVidPid;
+
     // The associations between input ports and display ports.
     // Used to determine which DisplayViewport should be tied to which InputDevice.
     std::unordered_map<std::string, uint8_t> portAssociations;

services/inputflinger/reader/InputReader.cpp
@@ -361,6 +361,7 @@ void InputReader::handleConfigurationChangedLocked(nsecs_t when) {
 void InputReader::refreshConfigurationLocked(uint32_t changes) {
     mPolicy->getReaderConfiguration(&mConfig);
     mEventHub->setExcludedDevices(mConfig.excludedDeviceNames);
+    mEventHub->setExcludedDevices(mConfig.excludedDeviceVidPid);
 
     if (!changes) return;
 

LineageOS/android_frameworks_native

Test: TODO

services/inputflinger/include/InputReaderBase.h
@@ -110,6 +110,10 @@ struct InputReaderConfiguration {
     // Devices with these names will be ignored.
     std::vector<std::string> excludedDeviceNames;
 
+    // The excluded device vid/pid for the platform.
+    // Devices with these vid/pid combo will be ignored.
+    std::vector<std::pair<uint16_t, uint16_t>> excludedDevicesVidPid;
+
     // The associations between input ports and display ports.
     // Used to determine which DisplayViewport should be tied to which InputDevice.
     std::unordered_map<std::string, uint8_t> portAssociations;

services/inputflinger/reader/InputReader.cpp
@@ -395,6 +395,7 @@ void InputReader::handleConfigurationChangedLocked(nsecs_t when) {
 void InputReader::refreshConfigurationLocked(ConfigurationChanges changes) {
     mPolicy->getReaderConfiguration(&mConfig);
     mEventHub->setExcludedDevices(mConfig.excludedDeviceNames);
+    mEventHub->setExcludedDevices(mConfig.excludedDevicesVidPid);
 
     using Change = InputReaderConfiguration::Change;
     if (!changes.any()) return;