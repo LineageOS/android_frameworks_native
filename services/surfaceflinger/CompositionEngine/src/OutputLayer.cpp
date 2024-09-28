b7dae29b25b3ac8f1f8998cbb5b840a7ae55fc1f
pjgowtham <pjgowtham@gmail.com>
2024-09-28 08:29:43 +0530

    surfaceflinger: Allow ZOrder override for UDFPS dim layer
    Change-Id: I367b2c395aeb8cb7cb9e3cc38eca98477befe007

--- services/surfaceflinger/CompositionEngine/src/OutputLayer.cpp
+++ services/surfaceflinger/CompositionEngine/src/OutputLayer.cpp
@@ -574,8 +574,10 @@ void OutputLayer::writeOutputDependentGeometryStateToHWC(HWC2::Layer* hwcLayer,
         (strncmp(getLayerFE().getDebugName(), UDFPS_BIOMETRIC_PROMPT_LAYER_NAME,
                  strlen(UDFPS_BIOMETRIC_PROMPT_LAYER_NAME)) == 0)) {
         z_udfps = getUdfpsZOrder(z, false);
-    } else if (strncmp(getLayerFE().getDebugName(), UDFPS_TOUCHED_LAYER_NAME,
-                       strlen(UDFPS_TOUCHED_LAYER_NAME)) == 0) {
+    } else if (strncmp(getLayerFE().getDebugName(), UDFPS_DIM_LAYER_NAME,
+                       strlen(UDFPS_DIM_LAYER_NAME)) == 0) {
+        z_udfps = getUdfpsDimZOrder(z);
+    } else if (strstr(getLayerFE().getDebugName(), UDFPS_TOUCHED_LAYER_NAME) != nullptr) {
         z_udfps = getUdfpsZOrder(z, true);
     }
 
