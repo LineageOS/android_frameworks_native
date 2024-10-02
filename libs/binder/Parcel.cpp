d2cf49c4deecf0b7dee9f293128c01e2f704e32d
Steven Moreland <smoreland@google.com>
2024-10-02 00:37:59 +0000

    libbinder: Parcel: validate read data before write
    This is slow, but it's required to prevent memory
    corruption.
    
    Ignore-AOSP-First: security
    Bug: 370840874
    Test: fuzzer
    (cherry picked from commit c54dad65317f851ce9d016bd90ec6a7a04da09fc)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:f946e3c5e34539ba1fe9f03025005e5b248ebbc6)
    Merged-In: Ibc5566ade0389221690dc90324f93394cf7fc9a5
    Change-Id: Ibc5566ade0389221690dc90324f93394cf7fc9a5

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -888,6 +888,10 @@ restart_write:
         //printf("Writing %ld bytes, padded to %ld\n", len, padded);
         uint8_t* const data = mData+mDataPos;
 
+        if (status_t status = validateReadData(mDataPos + padded); status != OK) {
+            return nullptr; // drops status
+        }
+
         // Need to pad at end?
         if (padded != len) {
 #if BYTE_ORDER == BIG_ENDIAN
@@ -1405,6 +1409,10 @@ status_t Parcel::writeObject(const flat_binder_object& val, bool nullMetaData)
     const bool enoughObjects = mObjectsSize < mObjectsCapacity;
     if (enoughData && enoughObjects) {
 restart_write:
+        if (status_t status = validateReadData(mDataPos + sizeof(val)); status != OK) {
+            return status;
+        }
+
         *reinterpret_cast<flat_binder_object*>(mData+mDataPos) = val;
 
         // remember if it's a file descriptor
@@ -1621,6 +1629,10 @@ status_t Parcel::writeAligned(T val) {
 
     if ((mDataPos+sizeof(val)) <= mDataCapacity) {
 restart_write:
+        if (status_t status = validateReadData(mDataPos + sizeof(val)); status != OK) {
+            return status;
+        }
+
         memcpy(mData + mDataPos, &val, sizeof(val));
         return finishWrite(sizeof(val));
     }
