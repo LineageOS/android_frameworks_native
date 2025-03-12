474f03e8b7d06484e9af9dce7af6db3efffc6487
Devin Moore <devinmoore@google.com>
2025-03-12 19:35:53 +0000

    Check mDataPos to see if the Parcel needs to grow
    Flag: EXEMPT bug fix
    Ignore-AOSP-First: security fix
    Test: atest binderUnitTest
    Bug: 399155883
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:f0b1c904a607f4dea4f35e3e9bc53e25f059f50e)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:5a247775d486c997f49b900477b8fd1c3eaba263)
    (cherry picked from commit 67024077a81b24033bc5f5f089b1870990b22512)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:089f57d23bc95ba5ebdee27869442b045b69896a
    Merged-In: I38b755ca3381cfca3300292873f763823fbf169b
    Change-Id: I38b755ca3381cfca3300292873f763823fbf169b

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -530,7 +530,7 @@ status_t Parcel::appendFrom(const Parcel *parcel, size_t offset, size_t len)
     }
     int numObjects = lastIndex - firstIndex + 1;
 
-    if ((mDataSize+len) > mDataCapacity) {
+    if ((mDataPos + len) > mDataCapacity) {
         // grow data
         err = growData(len);
         if (err != NO_ERROR) {
