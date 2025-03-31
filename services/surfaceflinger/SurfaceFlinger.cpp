e9740a47111cdeee41a36628d35a8a553de4dd59
opensourcefreak <john@snurr.nu>
2025-03-31 17:17:28 +0200

    SurfaceFlinger: Add support for overriding the rotation of non-primary displays
    Change-Id: I5b3fce7c9315975db262a66d7aac2818016d3e4f

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -3030,6 +3030,15 @@ ui::Rotation SurfaceFlinger::getPhysicalDisplayOrientation(DisplayId displayId,
                 break;
         }
     }
+
+    std::string orientation = base::GetProperty("ro.surface_flinger.secondary_display_orientation", "");
+    if ("ORIENTATION_90" == orientation)
+        return ui::ROTATION_90;
+    else if ("ORIENTATION_180" == orientation)
+        return ui::ROTATION_180;
+    else if ("ORIENTATION_270" == orientation)
+        return ui::ROTATION_270;
+
     return ui::ROTATION_0;
 }
 
