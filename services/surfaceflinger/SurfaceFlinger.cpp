578f734855cd5e326d1c98b22eb0793d9896e299
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-09-12 17:09:52 +0200

    Only rotate secondary internal displays
    Change-Id: Ia8248508c4f292779ff608605757fab2f9019dff

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -3254,6 +3254,10 @@ ui::Rotation SurfaceFlinger::getPhysicalDisplayOrientation(PhysicalDisplayId dis
         }
     }
 
+    const bool isInternalDisplay = mPhysicalDisplays.get(displayId)
+                                           .transform(&PhysicalDisplay::isInternal)
+                                           .value_or(false);
+
     if (isPrimary) {
         using Values = SurfaceFlingerProperties::primary_display_orientation_values;
         switch (primary_display_orientation(Values::ORIENTATION_0)) {
@@ -3266,7 +3270,7 @@ ui::Rotation SurfaceFlinger::getPhysicalDisplayOrientation(PhysicalDisplayId dis
             default:
                 break;
         }
-    } else {
+    } else if (isInternalDisplay) {
         using Values = SurfaceFlingerProperties::secondary_display_orientation_values;
         switch (secondary_display_orientation(Values::ORIENTATION_0)) {
             case Values::ORIENTATION_90:
