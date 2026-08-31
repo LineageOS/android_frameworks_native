54acec56dcbcc873ba894470c46b3136d4cebc9d
ExtremeXT <extremext@lineageos.org>
2026-08-31 14:18:28 +0300

    fixup! surfaceflinger: Allow ZOrder override for UDFPS dim layer
    On LineageOS 24.0, the dim layer's name is "VRI-Dim Layer for UDFPS",
    hence the zOrder modifier was never applied.
    
    Co-authored-by: borbelyvince <borbely.vince.11@gmail.com>
    Change-Id: I31f7d8bc25e82fd34e16fe74f74a621fce64f1b6
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- services/surfaceflinger/CompositionEngine/include/compositionengine/UdfpsExtension.h
+++ services/surfaceflinger/CompositionEngine/include/compositionengine/UdfpsExtension.h
@@ -20,7 +20,7 @@
 #define __UDFPS_EXTENSION__H__
 
 #define UDFPS_BIOMETRIC_PROMPT_LAYER_NAME "BiometricPrompt"
-#define UDFPS_DIM_LAYER_NAME "Dim Layer for UDFPS"
+#define UDFPS_DIM_LAYER_NAME "VRI-Dim Layer for UDFPS"
 #define UDFPS_LAYER_NAME "UdfpsControllerOverlay"
 #define UDFPS_TOUCHED_LAYER_NAME " SurfaceView[UdfpsControllerOverlay](BLAST)"
 
