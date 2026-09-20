VoltageOS/frameworks_native

commit c151a1b6b15687a2defc0f6607b1c76fd4a63362

BufferQueues: Add Surface-backed hidl 2.0 impl
Several clients still use HIDL 2.0, like media.

Future proof them by adding a compatible implementation of this
interface that doesn't use IGBP at all.

Lots of new tests to make sure the logic matches the spec of the HAL.

Also included in Surface is a new bit to toggle that disables the
automatic setting of generation numbers on buffers, which breaks
compatibility with the HAL

Bug: 393639175
Bug: 475852888
Flag: com.android.graphics.libgui.flags.wb_media_migration
Test: new tests

Change-Id: Ia36340444cca10c2a1bab6e19be1c9fa33ca421c

libs/gui/Surface.cpp
@@ -251,6 +251,11 @@ status_t Surface::setGenerationNumber(uint32_t generation) {
     return result;
 }
 
+void Surface::setAutoGenerationUpdate(bool autoGeneration) {
+    Mutex::Autolock lock(mMutex);
+    mAutoGenerationUpdate = autoGeneration;
+}
+
 uint64_t Surface::getNextFrameNumber() const {
     Mutex::Autolock lock(mMutex);
     return mNextFrameNumber;
@@ -2358,6 +2363,8 @@ int Surface::disconnect(int api, IGraphicBufferProducer::DisconnectMode mode) {
     mEnableFrameTimestamps = false;
     mMaxBufferCount = NUM_BUFFER_SLOTS;
     mLastReplacedFrameId = {};
+    mGenerationNumber = 0;
+    mAutoGenerationUpdate = true;
 
     if (api == NATIVE_WINDOW_API_CPU) {
         mConnectedToCpu = false;
@@ -2460,20 +2467,26 @@ int Surface::attachBuffer(ANativeWindowBuffer* buffer) {
 
 int Surface::attachBuffer(const sp<GraphicBuffer>& graphicBuffer) {
     ATRACE_CALL();
-    SURF_LOGV("Surface::attachBuffer bufferId=%" PRIu64, graphicBuffer->getId());
-
     Mutex::Autolock lock(mMutex);
+    SURF_LOGV("Surface::attachBuffer bufferId=%" PRIu64
+              " mAutoGenerationUpdate=%d, mGenerationNumber=%u, graphicBuffer generation number=%u",
+              graphicBuffer->getId(), mAutoGenerationUpdate, mGenerationNumber,
+              graphicBuffer->mGenerationNumber);
     if (mReportRemovedBuffers) {
         mRemovedBuffers.clear();
     }
 
     uint32_t priorGeneration = graphicBuffer->mGenerationNumber;
-    graphicBuffer->mGenerationNumber = mGenerationNumber;
+    if (mAutoGenerationUpdate) {
+        graphicBuffer->mGenerationNumber = mGenerationNumber;
+    }
     int32_t attachedSlot = -1;
     status_t result = mGraphicBufferProducer->attachBuffer(&attachedSlot, graphicBuffer);
     if (result != NO_ERROR) {
         SURF_LOGE("attachBuffer: IGraphicBufferProducer call failed (%d)", result);
-        graphicBuffer->mGenerationNumber = priorGeneration;
+        if (mAutoGenerationUpdate) {
+            graphicBuffer->mGenerationNumber = priorGeneration;
+        }
         return result;
     }
     if (mReportRemovedBuffers && (mSlots[attachedSlot].buffer != nullptr)) {

libs/gui/tests/Surface_test.cpp
@@ -360,6 +360,57 @@ TEST_F(SurfaceTest, SettingGenerationNumber) {
     ASSERT_EQ(1U, graphicBuffer->getGenerationNumber());
 }
 
+TEST_F(SurfaceTest, AutoGenerationUpdate) {
+    auto [consumer, surface] = BufferItemConsumer::create(GRALLOC_USAGE_SW_READ_OFTEN);
+
+    // Allocate a buffer.
+    sp<GraphicBuffer> buffer;
+    sp<Fence> fence;
+    ASSERT_EQ(NO_ERROR, surface->connect(NATIVE_WINDOW_API_CPU, nullptr));
+    ASSERT_EQ(NO_ERROR, surface->dequeueBuffer(&buffer, &fence));
+    ASSERT_EQ(NO_ERROR, surface->cancelBuffer(buffer, fence));
+
+    // Detach the buffer and check its generation number.
+    sp<GraphicBuffer> graphicBuffer;
+    ASSERT_EQ(NO_ERROR, surface->detachNextBuffer(&graphicBuffer, &fence));
+    ASSERT_EQ(0U, graphicBuffer->getGenerationNumber());
+
+    // Auto-generation is on by default. Attaching should update the generation number.
+    ASSERT_EQ(NO_ERROR, surface->setGenerationNumber(1));
+    ASSERT_EQ(NO_ERROR, surface->attachBuffer(graphicBuffer));
+    ASSERT_EQ(NO_ERROR, surface->cancelBuffer(graphicBuffer, fence));
+    ASSERT_EQ(NO_ERROR, surface->dequeueBuffer(&buffer, &fence));
+    ASSERT_EQ(1U, buffer->getGenerationNumber());
+    ASSERT_EQ(NO_ERROR, surface->cancelBuffer(buffer, fence));
+    ASSERT_EQ(NO_ERROR, surface->detachNextBuffer(&graphicBuffer, &fence));
+    ASSERT_EQ(1U, graphicBuffer->getGenerationNumber());
+
+    // Turn auto-generation off. Attaching should not update the generation number. And,
+    // importantly, attaching should fail for generation number mismatch.
+    surface->setAutoGenerationUpdate(false);
+    ASSERT_EQ(NO_ERROR, surface->setGenerationNumber(2));
+    ASSERT_EQ(BAD_VALUE, surface->attachBuffer(graphicBuffer));
+
+    graphicBuffer->setGenerationNumber(2);
+    ASSERT_EQ(NO_ERROR, surface->attachBuffer(graphicBuffer));
+    ASSERT_EQ(NO_ERROR, surface->cancelBuffer(graphicBuffer, fence));
+
+    ASSERT_EQ(NO_ERROR, surface->dequeueBuffer(&buffer, &fence));
+    ASSERT_EQ(2U, buffer->getGenerationNumber());
+    ASSERT_EQ(NO_ERROR, surface->cancelBuffer(buffer, fence));
+    ASSERT_EQ(NO_ERROR, surface->detachNextBuffer(&graphicBuffer, &fence));
+    ASSERT_EQ(2U, graphicBuffer->getGenerationNumber());
+
+    // Turn auto-generation back on. Attaching should update the generation number again.
+    surface->setAutoGenerationUpdate(true);
+    ASSERT_EQ(NO_ERROR, surface->setGenerationNumber(3));
+    ASSERT_EQ(NO_ERROR, surface->attachBuffer(graphicBuffer));
+    ASSERT_EQ(NO_ERROR, surface->cancelBuffer(graphicBuffer, fence));
+    ASSERT_EQ(NO_ERROR, surface->dequeueBuffer(&buffer, &fence));
+    ASSERT_EQ(3U, buffer->getGenerationNumber());
+    ASSERT_EQ(NO_ERROR, surface->cancelBuffer(buffer, fence));
+}
+
 TEST_F(SurfaceTest, GetConsumerName) {
     sp<IGraphicBufferProducer> producer;
     sp<IGraphicBufferConsumer> consumer;

libs/gui/Android.bp
@@ -338,6 +338,7 @@ filegroup {
         "bufferqueue/1.0/H2BGraphicBufferProducer.cpp",
         "bufferqueue/2.0/B2HProducerListener.cpp",
         "bufferqueue/2.0/H2BGraphicBufferProducer.cpp",
+        "bufferqueue/2.0/Surface2HGraphicBufferProducer.cpp",
     ],
 }
 

libs/gui/include/gui/Surface.h
@@ -255,6 +255,14 @@ public:
      * See IGBP::setGenerationNumber for more information. */
     status_t setGenerationNumber(uint32_t generationNumber);
 
+    /*
+     * Set whether the Surface should automatically update the generation number
+     * on any buffers attached to it after this call.
+     *
+     * Default is true.
+     */
+    void setAutoGenerationUpdate(bool autoGeneration);
+
     // See IGraphicBufferProducer::getConsumerName
     String8 getConsumerName() const;
 
@@ -803,6 +811,10 @@ protected:
     // Stores the current generation number. See setGenerationNumber and
     // IGraphicBufferProducer::setGenerationNumber for more information.
     uint32_t mGenerationNumber;
+
+    // If true, the generation number is automatically updated on any buffers
+    // attached to this surface.
+    bool mAutoGenerationUpdate = true;
 
     // Caches the values that have been passed to the producer.
     bool mSharedBufferMode;

libs/gui/include/gui/bufferqueue/2.0/Surface2HGraphicBufferProducer.h
@@ -1,0 +1,117 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <android/hardware/graphics/bufferqueue/2.0/IGraphicBufferProducer.h>
+#include <gui/Surface.h>
+#include <gui/bufferqueue/2.0/types.h>
+#include <hidl/HidlSupport.h>
+
+#include <mutex>
+
+namespace android::hardware::graphics::bufferqueue::V2_0::utils {
+
+using HGraphicBufferProducer =
+        ::android::hardware::graphics::bufferqueue::V2_0::IGraphicBufferProducer;
+using BGraphicBufferProducer = ::android::IGraphicBufferProducer;
+using HProducerListener = ::android::hardware::graphics::bufferqueue::V2_0::IProducerListener;
+
+using ::android::hardware::hidl_handle;
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::Return;
+using ::android::hardware::graphics::common::V1_2::HardwareBuffer;
+
+/**
+ * A wrapper around IGraphicBufferProducer that is backed by a Surface.
+ */
+class Surface2HGraphicBufferProducer : public HGraphicBufferProducer {
+public:
+    Surface2HGraphicBufferProducer(const sp<Surface>& base);
+
+    virtual Return<HStatus> setMaxDequeuedBufferCount(int32_t maxDequeuedBuffers) override;
+
+    virtual Return<void> requestBuffer(int32_t slot, requestBuffer_cb _hidl_cb) override;
+
+    virtual Return<HStatus> setAsyncMode(bool async) override;
+
+    virtual Return<void> dequeueBuffer(DequeueBufferInput const& input,
+                                       dequeueBuffer_cb _hidl_cb) override;
+
+    virtual Return<HStatus> detachBuffer(int32_t slot) override;
+
+    virtual Return<void> detachNextBuffer(detachNextBuffer_cb _hidl_cb) override;
+
+    virtual Return<void> attachBuffer(HardwareBuffer const& buffer, uint32_t generationNumber,
+                                      attachBuffer_cb _hidl_cb) override;
+
+    virtual Return<void> queueBuffer(int32_t slot, QueueBufferInput const& input,
+                                     queueBuffer_cb _hidl_cb) override;
+
+    virtual Return<HStatus> cancelBuffer(int32_t slot, hidl_handle const& fence) override;
+
+    virtual Return<void> query(int32_t what, query_cb _hidl_cb) override;
+
+    virtual Return<void> connect(const sp<HProducerListener>& listener, HConnectionType api,
+                                 bool producerControlledByApp, connect_cb _hidl_cb) override;
+
+    virtual Return<HStatus> disconnect(HConnectionType api) override;
+
+    virtual Return<HStatus> allocateBuffers(uint32_t width, uint32_t height, uint32_t format,
+                                            uint64_t usage) override;
+
+    virtual Return<HStatus> allowAllocation(bool allow) override;
+
+    virtual Return<HStatus> setGenerationNumber(uint32_t generationNumber) override;
+
+    virtual Return<HStatus> setDequeueTimeout(int64_t timeoutNs) override;
+
+    virtual Return<uint64_t> getUniqueId() override;
+
+    virtual Return<void> getConsumerName(getConsumerName_cb _hidl_cb) override;
+
+private:
+    class HidlBridgeListener;
+    struct Obituary;
+
+    struct SlotInfo {
+        bool isDequeued;
+        sp<GraphicBuffer> buffer;
+    };
+
+    void onBuffersDiscarded(const std::vector<sp<GraphicBuffer>>& buffers);
+    void onRemoteDied();
+
+    int getSlotForBufferLocked(const sp<GraphicBuffer>& buffer) REQUIRES(mMutex);
+    int getFreeOrEvictableSlotLocked() REQUIRES(mMutex);
+    void resetSlotsLocked() REQUIRES(mMutex);
+    void clearSlotLocked(int slot) REQUIRES(mMutex);
+
+    mutable std::mutex mMutex;
+
+    // HGBPs can only manage 64 buffers at a time. Instead of juggling some nicer datastructures,
+    // we'll save space and cache hits by just iterating over a small array for all our operations.
+    SlotInfo mSlotToInfo[BufferQueueDefs::NUM_BUFFER_SLOTS] GUARDED_BY(mMutex);
+
+    // Stored on connect or when fetched. Used for debugging.
+    std::string mConsumerName GUARDED_BY(mMutex) = "unknown-not-connected";
+
+    sp<Surface> mBase;
+    sp<Obituary> mObituary GUARDED_BY(mMutex);
+};
+
+} // namespace android::hardware::graphics::bufferqueue::V2_0::utils

libs/gui/tests/Android.bp
@@ -79,6 +79,7 @@ cc_test {
         "RenderCommandBuffer_test.cpp",
         "StreamSplitter_test.cpp",
         "Surface_test.cpp",
+        "Surface2HGraphicBufferProducer_test.cpp",
         "SurfaceTextureClient_test.cpp",
         "SurfaceTextureFBO_test.cpp",
         "SurfaceTextureGL_test.cpp",

LineageOS/android_frameworks_native

commit 3b8db1fd35fdf5269755dd86e01462075731645e

Surface: Preserve generation number across disconnect
BufferQueue retains its generation across disconnects. Preserve Surface's
cached value to avoid attachBuffer() failures after reconnecting.

Fixes: c151a1b6b156 ("BufferQueues: Add Surface-backed hidl 2.0 impl")
Test: atest libgui_test:SurfaceTest#GenerationNumberSurvivesReconnect
Test: atest libgui_test:SurfaceTest#SettingGenerationNumber
Test: atest libgui_test:SurfaceTest#AutoGenerationUpdate
Test: HEVC playback across fullscreen/PiP transitions
Change-Id: I2cd55414089cc92fc2fe249973c253fb847a1db3

libs/gui/Surface.cpp
@@ -2428,7 +2428,6 @@ int Surface::disconnect(int api, IGraphicBufferProducer::DisconnectMode mode) {
     mEnableFrameTimestamps = false;
     mMaxBufferCount = NUM_BUFFER_SLOTS;
     mLastReplacedFrameId = {};
-    mGenerationNumber = 0;
     mAutoGenerationUpdate = true;
 
     if (api == NATIVE_WINDOW_API_CPU) {

libs/gui/tests/Surface_test.cpp
@@ -355,6 +355,29 @@ TEST_F(SurfaceTest, SettingGenerationNumber) {
     ASSERT_EQ(1U, graphicBuffer->getGenerationNumber());
 }
 
+TEST_F(SurfaceTest, GenerationNumberSurvivesReconnect) {
+    auto [consumer, surface] = BufferItemConsumer::create(GRALLOC_USAGE_SW_READ_OFTEN);
+
+    ASSERT_EQ(NO_ERROR, surface->connect(NATIVE_WINDOW_API_MEDIA, nullptr));
+    sp<GraphicBuffer> buffer;
+    sp<Fence> fence;
+    ASSERT_EQ(NO_ERROR, surface->dequeueBuffer(&buffer, &fence));
+    ASSERT_EQ(NO_ERROR, surface->cancelBuffer(buffer, fence));
+    ASSERT_EQ(NO_ERROR, surface->detachNextBuffer(&buffer, &fence));
+    ASSERT_EQ(0U, buffer->getGenerationNumber());
+
+    ASSERT_EQ(NO_ERROR, surface->setGenerationNumber(1));
+    surface->setAutoGenerationUpdate(false);
+    ASSERT_EQ(NO_ERROR, surface->disconnect(NATIVE_WINDOW_API_MEDIA));
+    ASSERT_EQ(NO_ERROR, surface->connect(NATIVE_WINDOW_API_MEDIA, nullptr));
+
+    // Reconnect must preserve the generation and re-enable automatic updates.
+    ASSERT_EQ(NO_ERROR, surface->attachBuffer(buffer));
+    ASSERT_EQ(1U, buffer->getGenerationNumber());
+    ASSERT_EQ(NO_ERROR, surface->cancelBuffer(buffer, fence));
+    ASSERT_EQ(NO_ERROR, surface->disconnect(NATIVE_WINDOW_API_MEDIA));
+}
+
 TEST_F(SurfaceTest, AutoGenerationUpdate) {
     auto [consumer, surface] = BufferItemConsumer::create(GRALLOC_USAGE_SW_READ_OFTEN);
 